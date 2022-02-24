3736f6501c0f65e8b7dda25e7f108f4b7bdb9dee
bbernays <ben@bernays.me>
2022-02-24 09:39:46 -1000

    fix: Handle Less than 1 gb of memory (#193)

--- helpers/limits.go
+++ helpers/limits.go
@@ -8,11 +8,18 @@ const GB_IN_BYTES uint64 = 1024 * 1024 * 1024
 const GO_ROUTINES_PER_GB uint64 = 250000
 
 func GetMaxGoRoutines() uint64 {
-	totalMemory := memory.TotalMemory()
+	return calculateGoRoutines(getMemory())
+}
+
+func getMemory() uint64 {
+	return memory.TotalMemory()
+}
+
+func calculateGoRoutines(totalMemory uint64) uint64 {
 	if totalMemory == 0 {
 		// assume we have 2 GB RAM
 		return GO_ROUTINES_PER_GB * 2
 	}
 	gb := float64(totalMemory) / float64(GB_IN_BYTES)
-	return uint64(gb) * GO_ROUTINES_PER_GB
+	return uint64(float64(GO_ROUTINES_PER_GB) * gb)
 }
---
+++ helpers/limits_test.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculateGoRoutines(t *testing.T) {
+
+	cases := []struct {
+		Name       string
+		Memory     uint64
+		GoRoutines uint64
+	}{
+		{Name: "Zero", Memory: uint64(0), GoRoutines: uint64(500000)},
+		{Name: "Below 1073741824", Memory: uint64(990498816), GoRoutines: uint64(230618)},
+		{Name: "At 1073741824", Memory: uint64(1073741824), GoRoutines: uint64(250000)},
+		{Name: "Above 1073741824", Memory: uint64(1573741824), GoRoutines: uint64(366415)},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			assert.Equal(t, int(tc.GoRoutines), int(calculateGoRoutines(tc.Memory)))
+		})
+	}
+}
