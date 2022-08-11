2c76bddd21a7b5f60711ce0aa7d7ab3623fd9fc7
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-08-11 13:18:44 +0300

    fix SourceSpec

--- specs/source.go
+++ specs/source.go
@@ -13,12 +13,12 @@ type SourceSpec struct {
 	// Path is the path in the registry
 	Path string `json:"path" yaml:"path"`
 	// Registry can be github,local,grpc. Might support things like https in the future.
-	Registry      string   `json:"registry" yaml:"registry"`
-	MaxGoRoutines uint64   `json:"max_goroutines" yaml:"max_goroutines"`
-	Tables        []string `json:"tables" yaml:"tables"`
-	SkipTables    []string `json:"skip_tables" yaml:"skip_tables"`
-	Destinations  []string `json:"destinations" yaml:"destinations"`
-	// Spec          yaml.Node `json:"spec" yaml:"spec"`
+	Registry      string    `json:"registry" yaml:"registry"`
+	MaxGoRoutines uint64    `json:"max_goroutines" yaml:"max_goroutines"`
+	Tables        []string  `json:"tables" yaml:"tables"`
+	SkipTables    []string  `json:"skip_tables" yaml:"skip_tables"`
+	Destinations  []string  `json:"destinations" yaml:"destinations"`
+	Spec          yaml.Node `json:"spec" yaml:"spec"`
 }
 
 func (s *SourceSpec) UnmarshalYAML(n *yaml.Node) error {
