6297f64f502952aef3517422c2ba57b63995b223
Andrii Romanenko <andrejjrom@gmail.com>
2022-09-20 12:14:06 +0300

    added json tag `-` to tests

--- codegen/golang_test.go
+++ codegen/golang_test.go
@@ -32,6 +32,7 @@ type testStruct struct {
 	TimeCol        time.Time  `json:"time_col,omitempty"`
 	TimePointerCol *time.Time `json:"time_pointer_col,omitempty"`
 	JSONTAG        *string    `json:"json_tag"`
+	JSONTAG_SKIP   *string    `json:"-"`
 	NOJSONTAG      *string
 	*embeddedStruct
 }
