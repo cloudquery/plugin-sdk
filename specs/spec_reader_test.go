32a7c3b02d324c15a4e0ad525b134020c4674e23
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-09-27 17:57:42 +0300

    feat!: SpecReader to support multiple files, dirs and yaml

--- specs/spec_reader_test.go
+++ specs/spec_reader_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 func TestLoadSpecs(t *testing.T) {
-	specReader, err := NewSpecReader("testdata")
+	specReader, err := NewSpecReader([]string{"testdata/gcp.yml", "testdata/dir"})
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(specReader.sources) != 1 {
-		t.Fatalf("got: %d expected: 1", len(specReader.sources))
+	if len(specReader.Sources) != 2 {
+		t.Fatalf("got: %d expected: 1", len(specReader.Sources))
 	}
-	if len(specReader.destinations) != 1 {
-		t.Fatalf("got: %d expected: 1", len(specReader.destinations))
+	if len(specReader.Destinations) != 2 {
+		t.Fatalf("got: %d expected: 2", len(specReader.Destinations))
 	}
 }
