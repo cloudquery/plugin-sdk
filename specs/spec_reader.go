2531708540298570a9d9711f05abc2d73cc34ddb
Yevgeny Pats <yev.pats@gmail.com>
2022-09-27 19:06:10 +0300

    feat!: SpecReader to support multiple files, dirs and yaml (#183)
    This was a requested feature by @bbernays to take a similar approach
    [kubectl
    appl](https://kubernetes.io/docs/reference/kubectl/cheatsheet/#kubectl-apply)
    so it can accept multiple files, directories and files with multiple
    yamls.
    
    This was observed as a use-case from multiple users.
    
    Also this introduce environment substitution in configs.
    
    Co-authored-by: Herman Schaaf <hermanschaaf@gmail.com>

--- specs/spec_reader.go
+++ specs/spec_reader.go
@@ -8,78 +8,90 @@ import (
 )
 
 type SpecReader struct {
-	sources      map[string]Source
-	destinations map[string]Destination
+	Sources      map[string]*Source
+	Destinations map[string]*Destination
 }
 
-func NewSpecReader(directory string) (*SpecReader, error) {
-	reader := SpecReader{
-		sources:      make(map[string]Source),
-		destinations: make(map[string]Destination),
-	}
-	files, err := os.ReadDir(directory)
+func (r *SpecReader) loadSpecsFromFile(path string) error {
+	data, err := os.ReadFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read directory %s: %w", directory, err)
+		return fmt.Errorf("failed to read file %s: %w", path, err)
 	}
+	data = []byte(os.ExpandEnv(string(data)))
 
-	configFilesCount := 0
+	// support multiple yamls in one file
+	for _, doc := range strings.Split(string(data), "\n---\n") {
+		var s Spec
+		if err := SpecUnmarshalYamlStrict([]byte(doc), &s); err != nil {
+			return fmt.Errorf("failed to unmarshal file %s: %w", path, err)
+		}
+		switch s.Kind {
+		case KindSource:
+			source := s.Spec.(*Source)
+			if r.Sources[source.Name] != nil {
+				return fmt.Errorf("duplicate source name %s", source.Name)
+			}
+			r.Sources[source.Name] = source
+		case KindDestination:
+			destination := s.Spec.(*Destination)
+			if r.Destinations[destination.Name] != nil {
+				return fmt.Errorf("duplicate destination name %s", destination.Name)
+			}
+			r.Destinations[destination.Name] = destination
+		default:
+			return fmt.Errorf("unknown kind %s", s.Kind)
+		}
+	}
+	return nil
+}
+
+func (r *SpecReader) loadSpecsFromDir(path string) error {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf("failed to read directory %s: %w", path, err)
+	}
 	for _, file := range files {
 		if !file.IsDir() && !strings.HasPrefix(file.Name(), ".") && strings.HasSuffix(file.Name(), ".yml") {
-			data, err := os.ReadFile(filepath.Join(directory, file.Name()))
-			if err != nil {
-				return nil, fmt.Errorf("failed to read file %s: %w", file.Name(), err)
+			if err := r.loadSpecsFromFile(filepath.Join(path, file.Name())); err != nil {
+				return err
 			}
-			var s Spec
-			if err := SpecUnmarshalYamlStrict(data, &s); err != nil {
-				return nil, fmt.Errorf("failed to unmarshal file %s: %w", file.Name(), err)
-			}
-			configFilesCount++
-			switch s.Kind {
-			case KindSource:
-				reader.sources[file.Name()] = *s.Spec.(*Source)
-			case KindDestination:
-				reader.destinations[file.Name()] = *s.Spec.(*Destination)
-			default:
-				return nil, fmt.Errorf("unknown kind %s", s.Kind)
-			}
-		}
-	}
-	if configFilesCount == 0 {
-		return nil, fmt.Errorf("no valid config files found in directory %s", directory)
-	}
-	return &reader, nil
-}
-
-func (s *SpecReader) GetSources() []Source {
-	sources := make([]Source, 0, len(s.sources))
-	for _, spec := range s.sources {
-		sources = append(sources, spec)
-	}
-	return sources
-}
-
-func (s *SpecReader) GetSourceByName(name string) *Source {
-	for _, spec := range s.sources {
-		if spec.Name == name {
-			return &spec
 		}
 	}
 	return nil
 }
 
-func (s *SpecReader) GetDestinations() []Destination {
-	destinations := make([]Destination, 0, len(s.destinations))
-	for _, spec := range s.destinations {
-		destinations = append(destinations, spec)
+func NewSpecReader(paths []string) (*SpecReader, error) {
+	reader := &SpecReader{
+		Sources:      make(map[string]*Source),
+		Destinations: make(map[string]*Destination),
 	}
-	return destinations
-}
-
-func (s *SpecReader) GetDestinationByName(name string) *Destination {
-	for _, spec := range s.destinations {
-		if spec.Name == name {
-			return &spec
+	for _, path := range paths {
+		file, err := os.Open(path)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open path %s: %w", path, err)
+		}
+		fileInfo, err := file.Stat()
+		if err != nil {
+			file.Close()
+			return nil, fmt.Errorf("failed to stat path %s: %w", path, err)
+		}
+		file.Close()
+		if fileInfo.IsDir() {
+			if err := reader.loadSpecsFromDir(path); err != nil {
+				return nil, err
+			}
+		} else {
+			if err := reader.loadSpecsFromFile(path); err != nil {
+				return nil, err
+			}
 		}
 	}
-	return nil
+
+	if len(reader.Sources) == 0 {
+		return nil, fmt.Errorf("expecting at least one source in: %v", paths)
+	}
+	if len(reader.Destinations) == 0 {
+		return nil, fmt.Errorf("expecting at least one destination in: %v", paths)
+	}
+	return reader, nil
 }
--- specs/spec_reader_test.go
+++ specs/spec_reader_test.go
@@ -5,14 +5,19 @@ import (
 )
 
 func TestLoadSpecs(t *testing.T) {
-	specReader, err := NewSpecReader("testdata")
+	specReader, err := NewSpecReader([]string{"testdata/gcp.yml", "testdata/dir"})
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(specReader.sources) != 1 {
-		t.Fatalf("got: %d expected: 1", len(specReader.sources))
+	if len(specReader.Sources) != 2 {
+		t.Fatalf("got: %d expected: 1", len(specReader.Sources))
 	}
-	if len(specReader.destinations) != 1 {
-		t.Fatalf("got: %d expected: 1", len(specReader.destinations))
+	if len(specReader.Destinations) != 2 {
+		t.Fatalf("got: %d expected: 2", len(specReader.Destinations))
+	}
+
+	_, err = NewSpecReader([]string{"testdata/gcp.yml", "testdata/gcp.yml"})
+	if err != nil && err.Error() != "duplicate source name gcp" {
+		t.Fatalf("got: %s expected: duplicate source name error", err)
 	}
 }
