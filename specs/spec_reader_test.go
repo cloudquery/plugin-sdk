6f3d2795bf73f54d8c23a2fabcc3880a1faef386
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-09-27 19:05:17 +0300

    Add more tests

--- specs/spec_reader_test.go
+++ specs/spec_reader_test.go
@@ -15,4 +15,9 @@ func TestLoadSpecs(t *testing.T) {
 	if len(specReader.Destinations) != 2 {
 		t.Fatalf("got: %d expected: 2", len(specReader.Destinations))
 	}
+
+	_, err = NewSpecReader([]string{"testdata/gcp.yml", "testdata/gcp.yml"})
+	if err != nil && err.Error() != "duplicate source name gcp" {
+		t.Fatalf("got: %s expected: duplicate source name error", err)
+	}
 }
