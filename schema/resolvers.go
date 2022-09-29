5ae38d0156bedade74c9aada9001664487ef290c
Erez Rokah <erezrokah@users.noreply.github.com>
2022-09-29 11:36:27 +0300

    feat!: Remove ParentIDResolver (#202)
    Fixes https://github.com/cloudquery/plugin-sdk/issues/197
    
    ---

--- schema/resolvers.go
+++ schema/resolvers.go
@@ -18,12 +18,6 @@ func PathResolver(path string) ColumnResolver {
 	}
 }
 
-// ParentIDResolver resolves the cq_id from the parent
-// if you want to reference the parent's primary keys use ParentResourceFieldResolver as required.
-func ParentIDResolver(_ context.Context, _ ClientMeta, r *Resource, c Column) error {
-	return r.Set(c.Name, r.Parent.ID())
-}
-
 // ParentColumnResolver resolves a column from the parent's table data, if name isn't set the column will be set to null
 func ParentColumnResolver(name string) ColumnResolver {
 	return func(_ context.Context, _ ClientMeta, r *Resource, c Column) error {
