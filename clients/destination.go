a05de4cbf89bac37f0ed3a16cf4dee6d19e12639
Yevgeny Pats <yev.pats@gmail.com>
2022-09-30 16:01:01 +0300

    Update clients/destination.go
    Co-authored-by: Herman Schaaf <hermanschaaf@gmail.com>

--- clients/destination.go
+++ clients/destination.go
@@ -42,7 +42,7 @@ func NewDestinationClient(cc grpc.ClientConnInterface) *DestinationClient {
 
 // NewDestinationClientSpawn starts a new destination plugin process, connects to it via gRPC server
 // and returns a new DestinationClient
-func NewDestinationClientSpawn(ctx context.Context, path string, opts ...DestinationClientOption) (*DestinationClient, error) {
+func NewDestinationClient(ctx context.Context, path string, opts ...DestinationClientOption) (*DestinationClient, error) {
 	c := &DestinationClient{
 		logger: log.Logger,
 	}
