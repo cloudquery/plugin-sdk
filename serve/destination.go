238612a528d600693c704904fc1c8db44320162c
Yevgeny Pats <yev.pats@gmail.com>
2022-10-06 15:26:00 +0300

    Update serve/destination.go
    Co-authored-by: Herman Schaaf <hermanschaaf@gmail.com>

--- serve/destination.go
+++ serve/destination.go
@@ -134,7 +134,7 @@ func newCmdDestinationServe(destination *destinationServe) *cobra.Command {
 					logger.Info().Str("address", listener.Addr().String()).Msg("Got interrupt. Destination plugin server shuting down")
 					s.Stop()
 				case <-ctx.Done():
-					logger.Info().Str("address", listener.Addr().String()).Msg("Context cancelled. Destination plugin server shuting down")
+					logger.Info().Str("address", listener.Addr().String()).Msg("Context cancelled. Destination plugin server shutting down")
 					s.Stop()
 				}
 			}()
