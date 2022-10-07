97c7306e01a10261048bd41bb2393648e5678786
Andrii Romanenko <andrejjrom@gmail.com>
2022-10-07 20:00:44 +0300

    linter fix

--- helpers/strings.go
+++ helpers/strings.go
@@ -34,9 +34,9 @@ func HasDuplicates(resources []string) bool {
 // into \n (unix)
 func NormalizeNewlines(d []byte) []byte {
 	// replace CR LF \r\n (windows) with LF \n (unix)
-	d = bytes.Replace(d, []byte{13, 10}, []byte{10}, -1)
+	d = bytes.ReplaceAll(d, []byte{13, 10}, []byte{10})
 	// replace CF \r (mac) with LF \n (unix)
-	d = bytes.Replace(d, []byte{13}, []byte{10}, -1)
+	d = bytes.ReplaceAll(d, []byte{13}, []byte{10})
 	return d
 }
 
