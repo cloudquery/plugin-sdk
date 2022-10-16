c3daea6099a5571f2adac559be25149dafdb9a80
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-10-17 01:10:06 +0300

    random nits

--- serve/serve.go
+++ serve/serve.go
@@ -3,16 +3,8 @@ package serve
 import (
 	"time"
 
-	"github.com/cloudquery/plugin-sdk/plugins"
 	"google.golang.org/grpc/test/bufconn"
 )
-
-type Options struct {
-	// Required: Source or destination plugin to serve.
-	SourcePlugin      *plugins.SourcePlugin
-	DestinationPlugin plugins.DestinationPlugin
-	SentryDsn         string
-}
 
 const (
 	serveShort = `Start plugin server`
