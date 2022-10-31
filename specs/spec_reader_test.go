01be38953ced49f74651bc34cc2d7ac1426a4534
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2022-10-31 14:47:21 +0200

    add more tests to specreader

--- specs/spec_reader_test.go
+++ specs/spec_reader_test.go
@@ -4,20 +4,64 @@ import (
 	"testing"
 )
 
+type specLoaderTestCase struct {
+	name     string
+	path []string
+	err string
+	sources int
+	destinations int
+}
+
+var specLoaderTestCases = []specLoaderTestCase{
+	{
+		name: "sucess",
+		path: []string{"testdata/gcp.yml", "testdata/dir"},
+		err: "",
+		sources: 2,
+		destinations: 2,
+	},
+	{
+		name: "duplicate_source",
+		path: []string{"testdata/gcp.yml", "testdata/gcp.yml"},
+		err: "duplicate source name gcp",
+	},
+	{
+		name: "no_such_file",
+		path: []string{"testdata/dir/no_such_file.yml", "testdata/dir/postgresql.yml"},
+		err: "open testdata/dir/no_such_file.yml: no such file or directory",
+	},
+	{
+		name: "duplicate_destination",
+		path: []string{"testdata/dir/postgresql.yml", "testdata/dir/postgresql.yml"},
+		err: "duplicate destination name postgresql",
+	},
+	{
+		name: "different_versions_for_destinations",
+		path: []string{"testdata/gcp.yml", "testdata/gcpv2.yml"},
+		err: "destination postgresqlv2 is used by multiple sources cloudquery/gcp with different versions",
+	},
+}
+
 func TestLoadSpecs(t *testing.T) {
-	specReader, err := NewSpecReader([]string{"testdata/gcp.yml", "testdata/dir"})
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(specReader.Sources) != 2 {
-		t.Fatalf("got: %d expected: 1", len(specReader.Sources))
-	}
-	if len(specReader.Destinations) != 2 {
-		t.Fatalf("got: %d expected: 2", len(specReader.Destinations))
+	for _, tc := range specLoaderTestCases {
+		t.Run(tc.name, func(t *testing.T) {
+			specReader, err := NewSpecReader(tc.path)
+			if err != nil {
+				if err.Error() != tc.err {
+					t.Fatalf("expected error: '%s', got: '%s'", tc.err, err)
+				}
+				return
+			}
+			if tc.err != "" {
+				t.Fatalf("expected error: %s, got nil", tc.err)
+			}
+			if len(specReader.Sources) != tc.sources {
+				t.Fatalf("got: %d expected: %d", len(specReader.Sources), tc.sources)
+			}
+			if len(specReader.Destinations) != tc.destinations {
+				t.Fatalf("got: %d expected: %d", len(specReader.Destinations), tc.destinations)
+			}
+		})
 	}
 
-	_, err = NewSpecReader([]string{"testdata/gcp.yml", "testdata/gcp.yml"})
-	if err != nil && err.Error() != "duplicate source name gcp" {
-		t.Fatalf("got: %s expected: duplicate source name error", err)
-	}
 }
