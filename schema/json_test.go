a5672b5faa9f41f2179650f989761217575b3934
shimonp21 <37939765+shimonp21@users.noreply.github.com>
2022-11-25 13:16:47 +0200

    feat: Handle resolving of empty maps and slices (#430)
    <!--
    Explain what problem this PR addresses
    -->
    
    ---

--- schema/json_test.go
+++ schema/json_test.go
@@ -4,6 +4,10 @@ import (
 	"testing"
 )
 
+type Foo struct {
+	Num int
+}
+
 func TestJSONSet(t *testing.T) {
 	successfulTests := []struct {
 		source interface{}
@@ -13,8 +17,25 @@ func TestJSONSet(t *testing.T) {
 		{source: []byte("{}"), result: JSON{Bytes: []byte("{}"), Status: Present}},
 		{source: ([]byte)(nil), result: JSON{Status: Null}},
 		{source: (*string)(nil), result: JSON{Status: Null}},
+
 		{source: []int{1, 2, 3}, result: JSON{Bytes: []byte("[1,2,3]"), Status: Present}},
+		{source: []int(nil), result: JSON{Bytes: []byte(`[]`), Status: Present}},
+		{source: []int{}, result: JSON{Bytes: []byte(`[]`), Status: Present}},
+		{source: []Foo(nil), result: JSON{Bytes: []byte(`[]`), Status: Present}},
+		{source: []Foo{}, result: JSON{Bytes: []byte(`[]`), Status: Present}},
+		{source: []Foo{{1}}, result: JSON{Bytes: []byte(`[{"Num":1}]`), Status: Present}},
+
 		{source: map[string]interface{}{"foo": "bar"}, result: JSON{Bytes: []byte(`{"foo":"bar"}`), Status: Present}},
+		{source: map[string]interface{}(nil), result: JSON{Bytes: []byte(`{}`), Status: Present}},
+		{source: map[string]interface{}{}, result: JSON{Bytes: []byte(`{}`), Status: Present}},
+		{source: map[string]string{"foo": "bar"}, result: JSON{Bytes: []byte(`{"foo":"bar"}`), Status: Present}},
+		{source: map[string]string(nil), result: JSON{Bytes: []byte(`{}`), Status: Present}},
+		{source: map[string]string{}, result: JSON{Bytes: []byte(`{}`), Status: Present}},
+		{source: map[string]Foo{"foo": {1}}, result: JSON{Bytes: []byte(`{"foo":{"Num":1}}`), Status: Present}},
+		{source: map[string]Foo(nil), result: JSON{Bytes: []byte(`{}`), Status: Present}},
+		{source: map[string]Foo{}, result: JSON{Bytes: []byte(`{}`), Status: Present}},
+
+		{source: nil, result: JSON{Status: Null}},
 	}
 
 	for i, tt := range successfulTests {
