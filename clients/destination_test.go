03ab5d992b2cb76222aab82aaba3f73d39568ead
erezrokah <erezrokah@users.noreply.github.com>
2022-12-06 14:09:28 +0200

    test: Add test to ensure correct destination error is reported

--- clients/destination_test.go
+++ clients/destination_test.go
@@ -2,12 +2,17 @@ package clients
 
 import (
 	"context"
+	"encoding/json"
 	"os"
+	"path"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/specs"
 	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/require"
 )
 
 var newDestinationClientTestCases = []specs.Source{
@@ -58,4 +63,52 @@ func TestDestinationClient(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestDestinationClientWriteReturnsCorrectError(t *testing.T) {
+	ctx := context.Background()
+	l := zerolog.New(zerolog.NewTestWriter(t)).Output(zerolog.ConsoleWriter{Out: os.Stderr}).Level(zerolog.DebugLevel)
+	dirName := t.TempDir()
+	c, err := NewDestinationClient(ctx, specs.RegistryGithub, "cloudquery/sqlite", "v1.0.11", WithDestinationLogger(l), WithDestinationDirectory(dirName))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() {
+		if err := c.Terminate(); err != nil {
+			t.Logf("failed to terminate destination client: %v", err)
+		}
+	}()
+	sqliteSpec := struct {
+		connectionString string
+	}{connectionString: path.Join(dirName, "test.sql")}
+	if err := c.Initialize(ctx, specs.Destination{Spec: sqliteSpec}); err != nil {
+		t.Fatal(err)
+	}
+
+	name, err := c.Name(ctx)
+	if err != nil {
+		t.Fatal("failed to get name", err)
+	}
+
+	columns := []schema.Column{{Name: "int", Type: schema.TypeInt}}
+	tables := schema.Tables{&schema.Table{Name: "test-1", Columns: columns}, &schema.Table{Name: "test-2", Columns: columns}}
+	resource1 := schema.Resource{Item: map[string]interface{}{"int": 1}, Table: tables[0]}
+	destResource1, _ := json.Marshal(resource1.ToDestinationResource())
+	resource2 := schema.Resource{Item: map[string]interface{}{"int": 1}, Table: tables[1]}
+	destResource2, _ := json.Marshal(resource2.ToDestinationResource())
+	resourcesChannel := make(chan []byte)
+	go func() {
+		defer close(resourcesChannel)
+		// we need to stream enough data to the server so it at least starts processing it and return the relevant error
+		for i := 1; i < 100000; i++ {
+			resourcesChannel <- destResource1
+			resourcesChannel <- destResource2
+			resourcesChannel <- destResource1
+			resourcesChannel <- destResource2
+		}
+	}()
+
+	err = c.Write2(ctx, tables, name, time.Now().UTC(), resourcesChannel)
+	require.ErrorContains(t, err, "context canceled")
 }
