78b9acbfad4183506c39ea24a4634eb1ba70c04e
Erez Rokah <erezrokah@users.noreply.github.com>
2023-01-15 19:53:39 +0200

    feat(destinations): Add `migrate_mode` (#604)
    #### Summary
    
    <!-- 🎉 Thank you for making CloudQuery awesome by submitting a PR 🎉 -->
    
    Related to https://github.com/cloudquery/cloudquery/issues/6600, needed
    for https://github.com/cloudquery/cloudquery/pull/6763
    
    ---
    
    Use the following steps to ensure your PR is ready to be reviewed
    
    - [ ] Read the [contribution guidelines](../blob/main/CONTRIBUTING.md)
    🧑‍🎓
    - [ ] Run `go fmt` to format your code 🖊
    - [ ] Lint your changes via `golangci-lint run` 🚨 (install golangci-lint
    [here](https://golangci-lint.run/usage/install/#local-installation))
    - [ ] Update or add tests 🧪
    - [ ] Ensure the status checks below are successful ✅

--- specs/destination.go
+++ specs/destination.go
@@ -9,28 +9,17 @@ import (
 	"github.com/thoas/go-funk"
 )
 
-type WriteMode int
-
 type Destination struct {
-	Name           string    `json:"name,omitempty"`
-	Version        string    `json:"version,omitempty"`
-	Path           string    `json:"path,omitempty"`
-	Registry       Registry  `json:"registry,omitempty"`
-	WriteMode      WriteMode `json:"write_mode,omitempty"`
-	BatchSize      int       `json:"batch_size,omitempty"`
-	BatchSizeBytes int       `json:"batch_size_bytes,omitempty"`
-	Spec           any       `json:"spec,omitempty"`
+	Name           string      `json:"name,omitempty"`
+	Version        string      `json:"version,omitempty"`
+	Path           string      `json:"path,omitempty"`
+	Registry       Registry    `json:"registry,omitempty"`
+	WriteMode      WriteMode   `json:"write_mode,omitempty"`
+	MigrateMode    MigrateMode `json:"migrate_mode,omitempty"`
+	BatchSize      int         `json:"batch_size,omitempty"`
+	BatchSizeBytes int         `json:"batch_size_bytes,omitempty"`
+	Spec           any         `json:"spec,omitempty"`
 }
-
-const (
-	WriteModeOverwriteDeleteStale WriteMode = iota
-	WriteModeOverwrite
-	WriteModeAppend
-)
-
-var (
-	writeModeStrings = []string{"overwrite-delete-stale", "overwrite", "append"}
-)
 
 func (d *Destination) SetDefaults(defaultBatchSize, defaultBatchSizeBytes int) {
 	if d.Registry.String() == "" {
@@ -81,38 +70,4 @@ func (d *Destination) Validate() error {
 		return fmt.Errorf("batch_size must be greater than 0")
 	}
 	return nil
-}
-
-func (m WriteMode) String() string {
-	return writeModeStrings[m]
-}
-
-func (m WriteMode) MarshalJSON() ([]byte, error) {
-	buffer := bytes.NewBufferString(`"`)
-	buffer.WriteString(m.String())
-	buffer.WriteString(`"`)
-	return buffer.Bytes(), nil
-}
-
-func (m *WriteMode) UnmarshalJSON(data []byte) (err error) {
-	var writeMode string
-	if err := json.Unmarshal(data, &writeMode); err != nil {
-		return err
-	}
-	if *m, err = WriteModeFromString(writeMode); err != nil {
-		return err
-	}
-	return nil
-}
-
-func WriteModeFromString(s string) (WriteMode, error) {
-	switch s {
-	case "append":
-		return WriteModeAppend, nil
-	case "overwrite":
-		return WriteModeOverwrite, nil
-	case "overwrite-delete-stale":
-		return WriteModeOverwriteDeleteStale, nil
-	}
-	return 0, fmt.Errorf("invalid write mode: %s", s)
 }
