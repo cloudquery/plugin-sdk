6e31906bfd9bfe146242c4af35dff3e407afb981
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-01-16 18:01:40 +0200

    fmt

--- serve/source_v0_test.go
+++ serve/source_v0_test.go
@@ -4,78 +4,19 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"net"
 	"sync"
 	"testing"
 	"time"
 
-	clients "github.com/cloudquery/plugin-sdk/clients/source/v0"
+	clients "github.com/cloudquery/plugin-sdk/clients/source/v1"
 	"github.com/cloudquery/plugin-sdk/plugins/source"
 	"github.com/cloudquery/plugin-sdk/schema"
 	"github.com/cloudquery/plugin-sdk/specs"
-	"github.com/rs/zerolog"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type TestSourcePluginSpec struct {
-	Accounts []string `json:"accounts,omitempty" yaml:"accounts,omitempty"`
-}
-
-type testExecutionClient struct{}
-
-var _ schema.ClientMeta = &testExecutionClient{}
-
-var errTestExecutionClientErr = fmt.Errorf("error in newTestExecutionClientErr")
-
-var expectedExampleSpecConfig = specs.Spec{
-	Kind: specs.KindSource,
-	Spec: &specs.Source{
-		Name:    "testSourcePlugin",
-		Path:    "cloudquery/testSourcePlugin",
-		Version: "v1.0.0",
-		Tables:  []string{"*"},
-		Spec:    map[string]any{"accounts": []any{"all"}},
-	},
-}
-
-func testTable(name string) *schema.Table {
-	return &schema.Table{
-		Name: name,
-		Resolver: func(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- any) error {
-			res <- map[string]any{
-				"TestColumn": 3,
-			}
-			return nil
-		},
-		Columns: []schema.Column{
-			{
-				Name: "test_column",
-				Type: schema.TypeInt,
-			},
-		},
-	}
-}
-
-func (*testExecutionClient) ID() string {
-	return "testExecutionClient"
-}
-
-func newTestExecutionClient(context.Context, zerolog.Logger, specs.Source, source.Options) (schema.ClientMeta, error) {
-	return &testExecutionClient{}, nil
-}
-
-func newTestExecutionClientErr(context.Context, zerolog.Logger, specs.Source, source.Options) (schema.ClientMeta, error) {
-	return nil, errTestExecutionClientErr
-}
-
-func bufSourceDialer(context.Context, string) (net.Conn, error) {
-	testSourceListenerLock.Lock()
-	defer testSourceListenerLock.Unlock()
-	return testSourceListener.Dial()
-}
-
-func TestSourceSuccess(t *testing.T) {
+func TestSourceSuccessV1(t *testing.T) {
 	plugin := source.NewPlugin(
 		"testPlugin",
 		"v1.0.0",
@@ -149,7 +90,7 @@ func TestSourceSuccess(t *testing.T) {
 		t.Fatalf("Expected 2 tables but got %d", len(tables))
 	}
 
-	tables, err = c.GetTablesForSpec(ctx, &specs.Source{
+	if err := c.Init(ctx, specs.Source{
 		Name:         "testSourcePlugin",
 		Version:      "v1.0.0",
 		Path:         "cloudquery/testSourcePlugin",
@@ -157,24 +98,12 @@ func TestSourceSuccess(t *testing.T) {
 		Tables:       []string{"test_table"},
 		Spec:         TestSourcePluginSpec{Accounts: []string{"cloudquery/plugin-sdk"}},
 		Destinations: []string{"test"},
-	})
-	if err != nil {
+	}); err != nil {
 		t.Fatal(err)
 	}
-	if len(tables) != 1 {
-		t.Fatalf("Expected 1 table but got %d", len(tables))
-	}
+
 	resources := make(chan []byte, 2)
-	if err := c.Sync2(ctx,
-		specs.Source{
-			Name:         "testSourcePlugin",
-			Version:      "v1.0.0",
-			Path:         "cloudquery/testSourcePlugin",
-			Registry:     specs.RegistryGithub,
-			Tables:       []string{"test_table"},
-			Spec:         TestSourcePluginSpec{Accounts: []string{"cloudquery/plugin-sdk"}},
-			Destinations: []string{"test"},
-		},
+	if err := c.Sync(ctx,
 		resources); err != nil {
 		t.Fatal(err)
 	}
@@ -217,80 +146,6 @@ func TestSourceSuccess(t *testing.T) {
 
 	if clientStats.Panics != 0 {
 		t.Fatalf("Expected 0 panics but got %d", clientStats.Panics)
-	}
-
-	cancel()
-	wg.Wait()
-	if serverErr != nil {
-		t.Fatal(serverErr)
-	}
-}
-
-func TestSourceFail(t *testing.T) {
-	plugin := source.NewPlugin(
-		"testSourcePlugin",
-		"v1.0.0",
-		[]*schema.Table{testTable("test_table")},
-		newTestExecutionClientErr)
-
-	cmd := newCmdSourceRoot(&sourceServe{
-		plugin: plugin,
-	})
-	cmd.SetArgs([]string{"serve", "--network", "test"})
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	var wg sync.WaitGroup
-	wg.Add(1)
-	var serverErr error
-	go func() {
-		defer wg.Done()
-		serverErr = cmd.ExecuteContext(ctx)
-	}()
-	defer func() {
-		cancel()
-		wg.Wait()
-	}()
-	for {
-		testSourceListenerLock.Lock()
-		if testSourceListener != nil {
-			testSourceListenerLock.Unlock()
-			break
-		}
-		testSourceListenerLock.Unlock()
-		t.Log("waiting for grpc server to start")
-		time.Sleep(time.Millisecond * 200)
-	}
-
-	// https://stackoverflow.com/questions/42102496/testing-a-grpc-service
-	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufSourceDialer), grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
-	if err != nil {
-		t.Fatalf("Failed to dial bufnet: %v", err)
-	}
-	c, err := clients.NewClient(ctx, specs.RegistryGrpc, "", "", clients.WithSourceGRPCConnection(conn), clients.WithSourceNoSentry())
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		if err := c.Terminate(); err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	resources := make(chan []byte, 1)
-	err = c.Sync2(ctx,
-		specs.Source{
-			Name:         "testSourcePlugin",
-			Version:      "v1.0.0",
-			Path:         "cloudquery/testSourcePlugin",
-			Registry:     specs.RegistryGithub,
-			Tables:       []string{"*"},
-			Spec:         TestSourcePluginSpec{Accounts: []string{"cloudquery/plugin-sdk"}},
-			Destinations: []string{"test"},
-		},
-		resources)
-	close(resources)
-	if err == nil {
-		t.Fatal("expected error but got nil")
 	}
 
 	cancel()
