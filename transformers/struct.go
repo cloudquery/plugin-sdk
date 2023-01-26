235e0d83dd40a49552e10e2d6f8bfac5c841face
bbernays <ben@bernays.me>
2023-01-25 19:30:33 -0600

    Update struct.go

--- transformers/struct.go
+++ transformers/struct.go
@@ -151,7 +151,11 @@ func TransformWithStruct(st any, opts ...StructTransformerOption) schema.Transfo
 				}
 			}
 		}
-
+		// PKs can be defined in the struct tags or via the WithPrimaryKeys option, so the only heuristic we can apply
+		// is to validate that the total number of created PKs is not less than the total number of created PKs
+		if len(t.table.PrimaryKeys()) < len(t.pkFields) {
+			return fmt.Errorf("failed to create all of the desired primary keys: %v", t.pkFields)
+		}
 		return nil
 	}
 }
