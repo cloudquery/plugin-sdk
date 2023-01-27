d606e4f65f112f737a9da53a299bbef0aa9a63ab
Herman Schaaf <hermanschaaf@gmail.com>
2023-01-27 16:14:33 +0000

    Support downloading plugins from other cloudquery repos

--- registry/download.go
+++ registry/download.go
@@ -3,6 +3,7 @@ package registry
 import (
 	"archive/zip"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -10,8 +11,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"time"
-
-	"github.com/avast/retry-go/v4"
 
 	"github.com/schollz/progressbar/v3"
 )
@@ -30,10 +29,20 @@ func DownloadPluginFromGithub(ctx context.Context, localPath string, org string,
 	downloadDir := filepath.Dir(localPath)
 	pluginZipPath := localPath + ".zip"
 	// https://github.com/cloudquery/cloudquery/releases/download/plugins-source-test-v1.1.5/test_darwin_amd64.zip
-	downloadURL := fmt.Sprintf("https://github.com/cloudquery/cloudquery/releases/download/plugins-%s-%s-%s/%s_%s_%s.zip", typ, name, version, name, runtime.GOOS, runtime.GOARCH)
-	if org != "cloudquery" {
+	var urls []string
+	if org == "cloudquery" {
+		urls = []string{
+			// monorepo plugin
+			fmt.Sprintf("https://github.com/cloudquery/cloudquery/releases/download/plugins-%s-%s-%s/%s_%s_%s.zip", typ, name, version, name, runtime.GOOS, runtime.GOARCH),
+			// community plugin under CloudQuery org
+			fmt.Sprintf("https://github.com/cloudquery/cq-%s-%s/releases/download/%s/cq-%s-%s_%s_%s.zip", typ, name, version, typ, name, runtime.GOOS, runtime.GOARCH),
+		}
+	} else {
 		// https://github.com/yevgenypats/cq-source-test/releases/download/v1.0.1/cq-source-test_darwin_amd64.zip
-		downloadURL = fmt.Sprintf("https://github.com/%s/cq-%s-%s/releases/download/%s/cq-%s-%s_%s_%s.zip", org, typ, name, version, typ, name, runtime.GOOS, runtime.GOARCH)
+		urls = []string{
+			// community plugin under user org
+			fmt.Sprintf("https://github.com/%s/cq-%s-%s/releases/download/%s/cq-%s-%s_%s_%s.zip", org, typ, name, version, typ, name, runtime.GOOS, runtime.GOARCH),
+		}
 	}
 
 	if _, err := os.Stat(localPath); err == nil {
@@ -44,7 +53,7 @@ func DownloadPluginFromGithub(ctx context.Context, localPath string, org string,
 		return fmt.Errorf("failed to create plugin directory %s: %w", downloadDir, err)
 	}
 
-	err := downloadFile(ctx, pluginZipPath, downloadURL)
+	urlIndex, err := downloadFile(ctx, pluginZipPath, urls...)
 	if err != nil {
 		return fmt.Errorf("failed to download plugin: %w", err)
 	}
@@ -55,8 +64,10 @@ func DownloadPluginFromGithub(ctx context.Context, localPath string, org string,
 	}
 	defer archive.Close()
 
-	pathInArchive := fmt.Sprintf("plugins/%s/%s", typ, name)
-	if org != "cloudquery" {
+	var pathInArchive string
+	if org == "cloudquery" && urlIndex == 0 {
+		pathInArchive = fmt.Sprintf("plugins/%s/%s", typ, name)
+	} else {
 		pathInArchive = fmt.Sprintf("cq-%s-%s", typ, name)
 	}
 	pathInArchive = WithBinarySuffix(pathInArchive)
@@ -80,34 +91,37 @@ func DownloadPluginFromGithub(ctx context.Context, localPath string, org string,
 	return nil
 }
 
-func downloadFile(ctx context.Context, localPath string, url string) (err error) {
+func downloadFile(ctx context.Context, localPath string, urls ...string) (urlIndex int, err error) {
 	// Create the file
 	out, err := os.Create(localPath)
 	if err != nil {
-		return fmt.Errorf("failed to create file %s: %w", localPath, err)
+		return 0, fmt.Errorf("failed to create file %s: %w", localPath, err)
 	}
 	defer out.Close()
 
-	// Get the data
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		return fmt.Errorf("failed create request %s: %w", url, err)
-	}
+	for r := 0; r < RetryAttempts; r++ {
+		for i, url := range urls {
+			// Get the data
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+			if err != nil {
+				return 0, fmt.Errorf("failed create request %s: %w", url, err)
+			}
 
-	err = retry.Do(
-		func() error {
 			// Do http request
 			resp, err := http.DefaultClient.Do(req)
 			if err != nil {
-				return fmt.Errorf("failed to get url %s: %w", url, err)
+				return 0, fmt.Errorf("failed to get url %s: %w", url, err)
 			}
-
 			// Check server response
-			if resp.StatusCode != http.StatusOK {
+			if resp.StatusCode == http.StatusNotFound && i < len(urls)-1 {
+				// check alternative url
+				resp.Body.Close()
+				continue
+			} else if resp.StatusCode != http.StatusOK {
 				fmt.Printf("Failed downloading %s with status code %d. Retrying\n", url, resp.StatusCode)
-				return fmt.Errorf("statusCode != 200")
+				resp.Body.Close()
+				break
 			}
-			defer resp.Body.Close()
 
 			fmt.Printf("Downloading %s\n", url)
 			bar := downloadProgressBar(resp.ContentLength, "Downloading")
@@ -115,23 +129,15 @@ func downloadFile(ctx context.Context, localPath string, url string) (err error)
 			// Writer the body to file
 			_, err = io.Copy(io.MultiWriter(out, bar), resp.Body)
 			if err != nil {
-				return fmt.Errorf("failed to copy body to file %s: %w", localPath, err)
+				return 0, fmt.Errorf("failed to copy body to file %s: %w", localPath, err)
 			}
-
-			return nil
-		},
-		retry.RetryIf(func(err error) bool {
-			return err.Error() == "statusCode != 200"
-		}),
-		retry.Attempts(RetryAttempts),
-		retry.Delay(RetryWaitTime),
-	)
-
-	if err != nil {
-		return fmt.Errorf("failed downloading: %s", url)
+			resp.Body.Close()
+			return i, nil
+		}
+		time.Sleep(RetryWaitTime)
 	}
 
-	return nil
+	return 0, errors.New("failed to download plugin")
 }
 
 func downloadProgressBar(maxBytes int64, description ...string) *progressbar.ProgressBar {
