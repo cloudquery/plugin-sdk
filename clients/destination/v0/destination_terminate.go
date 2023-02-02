b30fae6e40f7d68b3c0b50ce4093996d8203815e
Herman Schaaf <hermanschaaf@gmail.com>
2023-02-02 12:44:41 +0000

    More info logs when plugins are being terminated

--- clients/destination/v0/destination_terminate.go
+++ clients/destination/v0/destination_terminate.go
@@ -13,10 +13,12 @@ func (c *Client) terminateProcess() error {
 		c.logger.Error().Err(err).Msg("failed to send interrupt signal to destination plugin")
 	}
 	timer := time.AfterFunc(5*time.Second, func() {
+		c.logger.Info().Msg("sending kill signal to destination plugin")
 		if err := c.cmd.Process.Kill(); err != nil {
 			c.logger.Error().Err(err).Msg("failed to kill destination plugin")
 		}
 	})
+	c.logger.Info().Msg("waiting for destination plugin to terminate")
 	st, err := c.cmd.Process.Wait()
 	timer.Stop()
 	if err != nil {
