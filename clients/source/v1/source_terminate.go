6f44e1c597b5ca2a31e21bd099c1f556d21bf2cf
Herman Schaaf <hermanschaaf@gmail.com>
2023-02-06 19:28:22 +0000

    fix(logging): Send more info logs when plugins are being terminated (#657)
    It's hard to tell from our current logs whether the source plugin got killed by another process or by our own. It's usually our own, but having a few more info logs around this event will make it unambiguous.

--- clients/source/v1/source_terminate.go
+++ clients/source/v1/source_terminate.go
@@ -13,10 +13,12 @@ func (c *Client) terminateProcess() error {
 		c.logger.Error().Err(err).Msg("failed to send interrupt signal to source plugin")
 	}
 	timer := time.AfterFunc(5*time.Second, func() {
+		c.logger.Info().Msg("sending kill signal to source plugin")
 		if err := c.cmd.Process.Kill(); err != nil {
 			c.logger.Error().Err(err).Msg("failed to kill source plugin")
 		}
 	})
+	c.logger.Info().Msg("waiting for source plugin to terminate")
 	st, err := c.cmd.Process.Wait()
 	timer.Stop()
 	if err != nil {
