902401202376a36fd4b6062325436ca924ca33f3
bbernays <ben@bernays.me>
2023-02-08 08:14:15 -0500

    Create unit_test_validators_test.go

---
+++ plugins/source/unit_test_validators_test.go
@@ -0,0 +1,38 @@
+package source
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/cloudquery/plugin-sdk/transformers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPluralNamesValidator(t *testing.T) {
+	testTable := []struct {
+		tableName string
+		err       error
+	}{
+		{
+			tableName: "test_tables",
+			err:       nil,
+		},
+		{
+			tableName: "test_table",
+			err:       fmt.Errorf("invalid table name: test_table. must be plural"),
+		},
+	}
+	for _, tc := range testTable {
+		tables := []*schema.Table{
+			{
+				Name:      tc.tableName,
+				Transform: transformers.TransformWithStruct(&testTable),
+			},
+		}
+
+		plugin := NewPlugin("testSourcePlugin", "1.0.0", tables, newTestExecutionClient)
+		err := validateTableNamePlural(t, plugin, nil)
+		assert.Equal(t, tc.err, err)
+	}
+}
