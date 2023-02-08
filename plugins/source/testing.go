d0898f1c1a81b74911a23bb425a31eca2d38e53d
bbernays <ben@bernays.me>
2023-02-08 08:14:10 -0500

    Update testing.go

--- plugins/source/testing.go
+++ plugins/source/testing.go
@@ -8,14 +8,12 @@ import (
 	"github.com/cloudquery/plugin-sdk/specs"
 )
 
-type Validator func(t *testing.T, plugin *Plugin, resources []*schema.Resource)
-
 func TestPluginSync(t *testing.T, plugin *Plugin, spec specs.Source, opts ...TestPluginOption) {
 	t.Helper()
 
 	o := &testPluginOptions{
 		parallel:   true,
-		validators: []Validator{validatePlugin},
+		validators: []Validator{validateColumnsHaveData, validateTableNamePlural},
 	}
 	for _, opt := range opts {
 		opt(o)
@@ -44,7 +42,10 @@ func TestPluginSync(t *testing.T, plugin *Plugin, spec specs.Source, opts ...Tes
 		t.Fatal(syncErr)
 	}
 	for _, validator := range o.validators {
-		validator(t, plugin, syncedResources)
+		err := validator(t, plugin, syncedResources)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
 
@@ -65,76 +66,4 @@ func WithTestPluginAdditionalValidators(v Validator) TestPluginOption {
 type testPluginOptions struct {
 	parallel   bool
 	validators []Validator
-}
-
-func getTableResources(t *testing.T, table *schema.Table, resources []*schema.Resource) []*schema.Resource {
-	t.Helper()
-
-	tableResources := make([]*schema.Resource, 0)
-
-	for _, resource := range resources {
-		if resource.Table.Name == table.Name {
-			tableResources = append(tableResources, resource)
-		}
-	}
-
-	return tableResources
-}
-
-func validateTable(t *testing.T, table *schema.Table, resources []*schema.Resource) {
-	t.Helper()
-	tableResources := getTableResources(t, table, resources)
-	if len(tableResources) == 0 {
-		t.Errorf("Expected table %s to be synced but it was not found", table.Name)
-		return
-	}
-	validateResources(t, tableResources)
-}
-
-func validatePlugin(t *testing.T, plugin *Plugin, resources []*schema.Resource) {
-	t.Helper()
-	tables := extractTables(plugin.tables)
-	for _, table := range tables {
-		validateTable(t, table, resources)
-	}
-}
-
-func extractTables(tables schema.Tables) []*schema.Table {
-	result := make([]*schema.Table, 0)
-	for _, table := range tables {
-		result = append(result, table)
-		result = append(result, extractTables(table.Relations)...)
-	}
-	return result
-}
-
-// Validates that every column has at least one non-nil value.
-// Also does some additional validations.
-func validateResources(t *testing.T, resources []*schema.Resource) {
-	t.Helper()
-
-	table := resources[0].Table
-
-	// A set of column-names that have values in at least one of the resources.
-	columnsWithValues := make([]bool, len(table.Columns))
-
-	for _, resource := range resources {
-		for i, value := range resource.GetValues() {
-			if value == nil {
-				continue
-			}
-			if value.Get() != nil && value.Get() != schema.Undefined {
-				columnsWithValues[i] = true
-			}
-		}
-	}
-
-	// Make sure every column has at least one value.
-	for i, hasValue := range columnsWithValues {
-		col := table.Columns[i]
-		emptyExpected := col.Name == "_cq_parent_id" && table.Parent == nil
-		if !hasValue && !emptyExpected && !col.IgnoreInTests {
-			t.Errorf("table: %s column %s has no values", table.Name, table.Columns[i].Name)
-		}
-	}
 }
