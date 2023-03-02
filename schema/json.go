3539900fd5b44f529a823af594869aae0f2d2049
erezrokah <erezrokah@users.noreply.github.com>
2023-03-02 13:37:14 +0200

    fix: Trim \n added by JSON encoder

--- schema/json.go
+++ schema/json.go
@@ -132,9 +132,10 @@ func (dst *JSON) Set(src any) error {
 			return err
 		}
 
-		buf := buffer.Bytes()
+		// JSON encoder adds a newline to the end of the output that we don't want.
+		buf := bytes.TrimSuffix(buffer.Bytes(), []byte("\n"))
 		// For map and slice jsons, it is easier for users to work with '[]' or '{}' instead of JSON's 'null'.
-		if bytes.Equal(buf, []byte("null\n")) {
+		if bytes.Equal(buf, []byte(`null`)) {
 			if isEmptyStringMap(value) {
 				*dst = JSON{Bytes: []byte("{}"), Status: Present}
 				return nil
