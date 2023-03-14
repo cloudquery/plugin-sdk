77bbb2ceaabca7ff9ca8d51b9a9faf58c1e671e8
bbernays <ben@bernays.me>
2023-03-14 16:20:25 -0500

    Update plugin_test.go

--- plugins/destination/plugin_test.go
+++ plugins/destination/plugin_test.go
@@ -7,40 +7,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func setupTables() schema.Table {
-	topLevelTable := schema.Table{
-		Name: "test_table",
-		Columns: []schema.Column{
-			schema.CqIDColumn,
-			schema.CqParentIDColumn,
-			{
-				Name: "id",
-				Type: schema.TypeUUID,
-				CreationOptions: schema.ColumnCreationOptions{
-					PrimaryKey: true,
-				},
-			},
-		},
-	}
-	nestedTable := schema.Table{
-		Name: "test_relation_table",
-		Columns: []schema.Column{
-			schema.CqIDColumn,
-			schema.CqParentIDColumn,
-			{
-				Name: "id",
-				Type: schema.TypeUUID,
-				CreationOptions: schema.ColumnCreationOptions{
-					PrimaryKey: true,
-				},
-			},
-		},
-		Parent: &topLevelTable,
-	}
-	topLevelTable.Relations = []*schema.Table{&nestedTable}
-	return topLevelTable
-}
-
 func TestSetCQIDAsPrimaryKeysForTables(t *testing.T) {
 	topLevelTable := schema.Table{
 		Name: "test_table",
