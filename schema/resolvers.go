31a5feecb49cef02560aa539316ed53ac31f2c23
bbernays <ben@bernays.me>
2023-03-21 15:13:28 -0500

    new resolver

--- schema/resolvers.go
+++ schema/resolvers.go
@@ -2,7 +2,9 @@ package schema
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/mitchellh/hashstructure/v2"
 	"github.com/thoas/go-funk"
 )
 
@@ -22,5 +24,15 @@ func PathResolver(path string) ColumnResolver {
 func ParentColumnResolver(name string) ColumnResolver {
 	return func(_ context.Context, _ ClientMeta, r *Resource, c Column) error {
 		return r.Set(c.Name, r.Parent.Get(name))
+	}
+}
+
+func ObjectHashResolve() ColumnResolver {
+	return func(_ context.Context, _ ClientMeta, r *Resource, c Column) error {
+		hash, err := hashstructure.Hash(r.Item, hashstructure.FormatV2, nil)
+		if err != nil {
+			return err
+		}
+		return r.Set(c.Name, fmt.Sprint(hash))
 	}
 }
