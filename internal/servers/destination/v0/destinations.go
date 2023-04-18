c8ef24e2ada3ea12952d948334013bd777d2d47f
Herman Schaaf <hermanschaaf@gmail.com>
2023-04-18 18:47:45 +0100

    Undo convertedResource.Release() for now

--- internal/servers/destination/v0/destinations.go
+++ internal/servers/destination/v0/destinations.go
@@ -141,7 +141,6 @@ func (s *Server) Write2(msg pb.Destination_Write2Server) error {
 		select {
 		case resources <- convertedResource:
 		case <-ctx.Done():
-			convertedResource.Release()
 			close(resources)
 			if err := eg.Wait(); err != nil {
 				return status.Errorf(codes.Internal, "Context done: %v and failed to wait for plugin: %v", ctx.Err(), err)
