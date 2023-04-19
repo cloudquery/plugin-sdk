ab049db9ac17313f897fa898dd22d9382e34bc8f
candiduslynx <candiduslynx@gmail.com>
2023-04-20 00:27:01 +0300

    deprecation notice

--- testdata/testdata.go
+++ testdata/testdata.go
@@ -246,6 +246,8 @@ func GenTestData(mem memory.Allocator, sc *arrow.Schema, opts GenTestDataOptions
 	return records
 }
 
+// GenTestDataV1 does approximately the same job as GenTestData, however, it's intended for simpler use-cases.
+// Deprecated. Will be removed in future release.
 func GenTestDataV1(table *schema.Table) schema.CQTypes {
 	data := make(schema.CQTypes, len(table.Columns))
 	for i, c := range table.Columns {
