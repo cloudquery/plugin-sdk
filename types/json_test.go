24de367443e40e538e264a126e98cd6d8c68307a
candiduslynx <candiduslynx@gmail.com>
2023-04-26 11:33:58 +0300

    add json tests

--- types/json_test.go
+++ types/json_test.go
@@ -104,3 +104,160 @@ func TestJSONBuilder_UnmarshalOne(t *testing.T) {
 		})
 	}
 }
+
+func TestJSONArray_GetOneForMarshal(t *testing.T) {
+	cases := []struct {
+		name string
+		data string
+		want json.RawMessage
+		nil  bool
+	}{
+		{
+			name: `map`,
+			data: `{"a": 1, "b": 2}`,
+			want: json.RawMessage(`{"a":1,"b":2}`),
+		},
+		{
+			name: `two maps`,
+			data: `{"a": 1, "b": 2}{"c": 3, "d": 4}`,
+			want: json.RawMessage(`{"a":1,"b":2}`),
+		},
+		{
+			name: `array`,
+			data: `[1, 2, 3]`,
+			want: json.RawMessage(`[1,2,3]`),
+		},
+		{
+			name: `two arrays`,
+			data: `[1, 2, 3][4, 5, 6]`,
+			want: json.RawMessage(`[1,2,3]`),
+		},
+		{
+			name: `null`,
+			data: `null`,
+			nil:  true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			mem := memory.NewCheckedAllocator(memory.NewGoAllocator())
+			defer mem.AssertSize(t, 0)
+			b := NewJSONBuilder(array.NewExtensionBuilder(mem, NewJSONType()))
+			defer b.Release()
+			dec := json.NewDecoder(bytes.NewReader([]byte(tc.data)))
+			err := b.UnmarshalOne(dec)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			a := b.NewArray()
+			defer a.Release()
+			if tc.nil {
+				require.Nil(t, a.GetOneForMarshal(0))
+			} else {
+				require.Exactly(t, tc.want, a.GetOneForMarshal(0))
+			}
+		})
+	}
+}
+func TestJSONArray_ValueStr(t *testing.T) {
+	cases := []struct {
+		name string
+		data string
+		want string
+		nil  bool
+	}{
+		{
+			name: `map`,
+			data: `{"a": 1, "b": 2}`,
+			want: `{"a":1,"b":2}`,
+		},
+		{
+			name: `two maps`,
+			data: `{"a": 1, "b": 2}{"c": 3, "d": 4}`,
+			want: `{"a":1,"b":2}`,
+		},
+		{
+			name: `array`,
+			data: `[1, 2, 3]`,
+			want: `[1,2,3]`,
+		},
+		{
+			name: `two arrays`,
+			data: `[1, 2, 3][4, 5, 6]`,
+			want: `[1,2,3]`,
+		},
+		{
+			name: `null`,
+			data: `null`,
+			want: array.NullValueStr,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			mem := memory.NewCheckedAllocator(memory.NewGoAllocator())
+			defer mem.AssertSize(t, 0)
+			b := NewJSONBuilder(array.NewExtensionBuilder(mem, NewJSONType()))
+			defer b.Release()
+			dec := json.NewDecoder(bytes.NewReader([]byte(tc.data)))
+			err := b.UnmarshalOne(dec)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			a := b.NewArray()
+			defer a.Release()
+			require.Exactly(t, tc.want, a.ValueStr(0))
+		})
+	}
+}
+
+func TestJSONArray_Value(t *testing.T) {
+	cases := []struct {
+		name string
+		data string
+		want any
+	}{
+		{
+			name: `map`,
+			data: `{"a": 1, "b": 2}`,
+			want: map[string]any{"a": float64(1), "b": float64(2)},
+		},
+		{
+			name: `two maps`,
+			data: `{"a": 1, "b": 2}{"c": 3, "d": 4}`,
+			want: map[string]any{"a": float64(1), "b": float64(2)},
+		},
+		{
+			name: `array`,
+			data: `[1, 2, 3]`,
+			want: []any{float64(1), float64(2), float64(3)},
+		},
+		{
+			name: `two arrays`,
+			data: `[1, 2, 3][4, 5, 6]`,
+			want: []any{float64(1), float64(2), float64(3)},
+		},
+		{
+			name: `null`,
+			data: `null`,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			mem := memory.NewCheckedAllocator(memory.NewGoAllocator())
+			defer mem.AssertSize(t, 0)
+			b := NewJSONBuilder(array.NewExtensionBuilder(mem, NewJSONType()))
+			defer b.Release()
+			dec := json.NewDecoder(bytes.NewReader([]byte(tc.data)))
+			err := b.UnmarshalOne(dec)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			a := b.NewArray().(*JSONArray)
+			defer a.Release()
+			require.Equal(t, tc.want, a.Value(0))
+		})
+	}
+}
