2e5634744d95972d43c929d3bf4af3536db57e88
candiduslynx <candiduslynx@gmail.com>
2023-04-27 17:41:25 +0300

    add extra json tests

--- types/json_test.go
+++ types/json_test.go
@@ -262,3 +262,116 @@ func TestJSONArray_Value(t *testing.T) {
 		})
 	}
 }
+
+func TestJSON_MarshalUnmarshal(t *testing.T) {
+	cases := []struct {
+		name string
+		data string
+		want []any
+	}{
+		{
+			name: `map`,
+			data: `[{"a":1,"b":2}]`,
+			want: []any{map[string]any{"a": float64(1), "b": float64(2)}},
+		},
+		{
+			name: `array`,
+			data: `[[1,2,3]]`,
+			want: []any{[]any{float64(1), float64(2), float64(3)}},
+		},
+		{
+			name: `empty`,
+			data: `[]`,
+		},
+		{
+			name: `mixed`,
+			data: `[{"a":1,"b":2},null,{"c":3,"d":4},null,{"e":5,"f":6},{"g":7,"h":8}]`,
+			want: []any{
+				map[string]any{"a": float64(1), "b": float64(2)},
+				nil,
+				map[string]any{"c": float64(3), "d": float64(4)},
+				nil,
+				map[string]any{"e": float64(5), "f": float64(6)},
+				map[string]any{"g": float64(7), "h": float64(8)},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			mem := memory.NewCheckedAllocator(memory.NewGoAllocator())
+			defer mem.AssertSize(t, 0)
+			b := NewJSONBuilder(array.NewExtensionBuilder(mem, NewJSONType()))
+			defer b.Release()
+			require.NoError(t, b.UnmarshalJSON([]byte(tc.data)))
+			a := b.NewArray().(*JSONArray)
+			defer a.Release()
+			data, err := a.MarshalJSON()
+			require.NoError(t, err)
+			require.Equal(t, tc.data, string(data))
+			require.Equal(t, len(tc.want), a.Len())
+			for i, elem := range tc.want {
+				require.Equal(t, elem, a.Value(i))
+			}
+		})
+	}
+
+}
+
+func TestJSON_FromToString(t *testing.T) {
+	cases := []struct {
+		name string
+		data []string
+		want []any
+	}{
+		{
+			name: `map`,
+			data: []string{`{"a":1,"b":2}`},
+			want: []any{map[string]any{"a": float64(1), "b": float64(2)}},
+		},
+		{
+			name: `array`,
+			data: []string{`[1,2,3]`},
+			want: []any{[]any{float64(1), float64(2), float64(3)}},
+		},
+		{
+			name: `empty`,
+			data: []string{`(null)`},
+			want: []any{nil},
+		},
+		{
+			name: `mixed`,
+			data: []string{`{"a":1,"b":2}`, `(null)`, `{"c":3,"d":4}`, `(null)`, `{"e":5,"f":6}`, `{"g":7,"h":8}`},
+			want: []any{
+				map[string]any{"a": float64(1), "b": float64(2)},
+				nil,
+				map[string]any{"c": float64(3), "d": float64(4)},
+				nil,
+				map[string]any{"e": float64(5), "f": float64(6)},
+				map[string]any{"g": float64(7), "h": float64(8)},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			require.Equalf(t, len(tc.want), len(tc.data), "want and data should be of the same length")
+
+			mem := memory.NewCheckedAllocator(memory.NewGoAllocator())
+			defer mem.AssertSize(t, 0)
+			b := NewJSONBuilder(array.NewExtensionBuilder(mem, NewJSONType()))
+			defer b.Release()
+			for _, str := range tc.data {
+				require.NoError(t, b.AppendValueFromString(str))
+			}
+			a := b.NewArray().(*JSONArray)
+			defer a.Release()
+
+			require.Equal(t, len(tc.want), a.Len())
+			for i, elem := range tc.want {
+				require.Equal(t, elem, a.Value(i))
+				require.Equal(t, tc.data[i], a.ValueStr(i))
+			}
+		})
+	}
+}
