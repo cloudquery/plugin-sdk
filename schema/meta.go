251bab854d0d73cb9c12aff02799ab2f4c7db96c
candiduslynx <candiduslynx@gmail.com>
2023-05-15 12:37:28 +0300

    remove redundant fields

--- schema/meta.go
+++ schema/meta.go
@@ -14,14 +14,14 @@ type ClientMeta interface {
 
 // These columns are managed and populated by the source plugins
 var CqIDColumn = Column{
-	Name:            CqIDField.Name,
-	Type:            CqIDField.Type,
+	Name:            "_cq_id",
+	Type:            types.ExtensionTypes.UUID,
 	Description:     "Internal CQ ID of the row",
 	CreationOptions: CreationOptions{NotNull: true, Unique: true},
 }
 var CqParentIDColumn = Column{
-	Name:          CqParentIDField.Name,
-	Type:          CqParentIDField.Type,
+	Name:          "_cq_parent_id",
+	Type:          types.ExtensionTypes.UUID,
 	Description:   "Internal CQ ID of the parent row",
 	Resolver:      parentCqUUIDResolver(),
 	IgnoreInTests: true,
@@ -29,36 +29,14 @@ var CqParentIDColumn = Column{
 
 // These columns are managed and populated by the destination plugin.
 var CqSyncTimeColumn = Column{
-	Name:        CqSyncTimeField.Name,
-	Type:        CqSyncTimeField.Type,
+	Name:        "_cq_sync_time",
+	Type:        arrow.FixedWidthTypes.Timestamp_us,
 	Description: "Internal CQ row of when sync was started (this will be the same for all rows in a single fetch)",
 }
 var CqSourceNameColumn = Column{
-	Name:        CqSourceNameField.Name,
-	Type:        CqSourceNameField.Type,
+	Name:        "_cq_source_name",
+	Type:        arrow.BinaryTypes.String,
 	Description: "Internal CQ row that references the source plugin name data was retrieved",
-}
-
-var CqIDField = arrow.Field{
-	Name:     "_cq_id",
-	Type:     types.ExtensionTypes.UUID,
-	Metadata: arrow.MetadataFrom(map[string]string{MetadataUnique: MetadataTrue}),
-}
-
-var CqParentIDField = arrow.Field{
-	Name:     "_cq_parent_id",
-	Type:     types.ExtensionTypes.UUID,
-	Metadata: arrow.MetadataFrom(map[string]string{MetadataUnique: MetadataTrue}),
-}
-
-var CqSyncTimeField = arrow.Field{
-	Name: "_cq_sync_time",
-	Type: arrow.FixedWidthTypes.Timestamp_us,
-}
-
-var CqSourceNameField = arrow.Field{
-	Name: "_cq_source_name",
-	Type: arrow.BinaryTypes.String,
 }
 
 func parentCqUUIDResolver() ColumnResolver {
