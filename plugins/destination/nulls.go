a8c1fdc94a07f9678c17a17e3568b417e9dab160
candiduslynx <candiduslynx@gmail.com>
2023-05-29 10:30:18 +0300

    replace with empty values

---
+++ plugins/destination/nulls.go
@@ -0,0 +1,73 @@
+package destination
+
+import (
+	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v13/arrow/array"
+	"github.com/apache/arrow/go/v13/arrow/memory"
+)
+
+func stripNullsFromLists(records []arrow.Record) {
+	for i := range records {
+		cols := make([]arrow.Array, records[i].NumCols())
+		for c := range records[i].Columns() {
+			if records[i].Column(c).DataType().ID() != arrow.LIST {
+				cols[c] = records[i].Column(c)
+				continue
+			}
+
+			list := records[i].Column(c).(*array.List)
+			bldr := array.NewListBuilder(memory.DefaultAllocator, list.DataType().(*arrow.ListType).Elem())
+			for j := 0; j < list.Len(); j++ {
+				if list.IsNull(j) {
+					bldr.AppendNull()
+					continue
+				}
+				bldr.Append(true)
+				vBldr := bldr.ValueBuilder()
+				from, to := list.ValueOffsets(j)
+				slc := array.NewSlice(list.ListValues(), from, to)
+				for k := 0; k < int(to-from); k++ {
+					if slc.IsNull(k) {
+						continue
+					}
+					err := vBldr.AppendValueFromString(slc.ValueStr(k))
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+			cols[c] = bldr.NewArray()
+		}
+		records[i] = array.NewRecord(records[i].Schema(), cols, records[i].NumRows())
+	}
+}
+
+type AllowNullFunc func(arrow.DataType) bool
+
+func (f AllowNullFunc) replaceNullsByEmpty(records []arrow.Record) {
+	if f == nil {
+		return
+	}
+	for i := range records {
+		cols := make([]arrow.Array, records[i].NumCols())
+		for c, col := range records[i].Columns() {
+			if col.NullN() == 0 || f(records[i].Column(c).DataType()) {
+				cols[c] = col
+				continue
+			}
+
+			builder := array.NewBuilder(memory.DefaultAllocator, records[i].Column(c).DataType())
+			for j := 0; j < col.Len(); j++ {
+				if col.IsNull(j) {
+					builder.AppendEmptyValue()
+					continue
+				}
+
+				if err := builder.AppendValueFromString(col.ValueStr(j)); err != nil {
+					panic(err)
+				}
+			}
+			cols[c] = builder.NewArray()
+		}
+	}
+}
