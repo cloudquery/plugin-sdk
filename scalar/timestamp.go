afdff8219af34ad7c6ff3021d9e9464ee882ecf2
Kemal Hadimli <disq@users.noreply.github.com>
2023-05-30 12:02:03 +0100

    Revert "Update scalar/timestamp.go"
    This reverts commit f6605323287076b121a3e32651654d924cb1a156.

--- scalar/timestamp.go
+++ scalar/timestamp.go
@@ -37,7 +37,18 @@ func (s *Timestamp) IsValid() bool {
 }
 
 func (s *Timestamp) DataType() arrow.DataType {
-	return &arrow.TimestampType{Unit: s.Unit}
+	switch s.Unit {
+	case arrow.Second:
+		return arrow.FixedWidthTypes.Timestamp_s
+	case arrow.Millisecond:
+		return arrow.FixedWidthTypes.Timestamp_ms
+	case arrow.Nanosecond:
+		return arrow.FixedWidthTypes.Timestamp_ns
+	case arrow.Microsecond:
+		return arrow.FixedWidthTypes.Timestamp_us
+	default:
+		panic("unknown timestamp unit")
+	}
 }
 
 func (s *Timestamp) Equal(rhs Scalar) bool {
