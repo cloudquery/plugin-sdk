ac2cfbdc09521ae78d648fe841354351348496cb
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2023-06-01 10:19:40 +0300

    fix(test): Use `array.WithUnorderedMapKeys` (#921)
    Blocked by https://github.com/apache/arrow/pull/35823
    Required by https://github.com/cloudquery/cloudquery/pull/11078

--- plugins/destination/plugin_testing.go
+++ plugins/destination/plugin_testing.go
@@ -2,7 +2,6 @@ package destination
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"sort"
 	"strings"
@@ -64,30 +63,6 @@ type PluginTestSuiteTests struct {
 
 	MigrateStrategyOverwrite MigrateStrategy
 	MigrateStrategyAppend    MigrateStrategy
-}
-
-func RecordDiff(l arrow.Record, r arrow.Record) string {
-	var sb strings.Builder
-	if l.NumCols() != r.NumCols() {
-		return fmt.Sprintf("different number of columns: %d vs %d", l.NumCols(), r.NumCols())
-	}
-	if l.NumRows() != r.NumRows() {
-		return fmt.Sprintf("different number of rows: %d vs %d", l.NumRows(), r.NumRows())
-	}
-	for i := 0; i < int(l.NumCols()); i++ {
-		edits, err := array.Diff(l.Column(i), r.Column(i))
-		if err != nil {
-			panic(fmt.Sprintf("left: %v, right: %v, error: %v", l.Column(i).DataType(), r.Column(i).DataType(), err))
-		}
-		diff := edits.UnifiedDiff(l.Column(i), r.Column(i))
-		if diff != "" {
-			sb.WriteString(l.Schema().Field(i).Name)
-			sb.WriteString(": ")
-			sb.WriteString(diff)
-			sb.WriteString("\n")
-		}
-	}
-	return sb.String()
 }
 
 func getTestLogger(t *testing.T) zerolog.Logger {
