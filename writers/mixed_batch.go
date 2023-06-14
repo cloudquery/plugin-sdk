1b79d012614a945052a4a180f37bce27a7b3ec68
Herman Schaaf <hermanschaaf@gmail.com>
2023-06-14 12:16:20 +0100

    Simplified mixed batch writer

--- writers/mixed_batch.go
+++ writers/mixed_batch.go
@@ -2,8 +2,6 @@ package writers
 
 import (
 	"context"
-	"reflect"
-	"sync"
 	"time"
 
 	"github.com/apache/arrow/go/v13/arrow/util"
@@ -34,11 +32,6 @@ type MixedBatchWriter struct {
 	batchTimeout   time.Duration
 	batchSize      int
 	batchSizeBytes int
-
-	workerCreateTable *mixedBatchWorker[plugin.MessageCreateTable]
-	workerInsert      *mixedBatchWorker[plugin.MessageInsert]
-	workerDeleteStale *mixedBatchWorker[plugin.MessageDeleteStale]
-	workersLock       *sync.Mutex
 }
 
 // Assert at compile-time that MixedBatchWriter implements the Writer interface
@@ -70,86 +63,14 @@ func WithMixedBatchWriterSizeBytes(size int) MixedBatchWriterOption {
 	}
 }
 
-type mixedBatchWorker[T plugin.Message] struct {
-	count     int
-	wg        *sync.WaitGroup
-	ch        chan T
-	flush     chan chan bool
-	messages  []T
-	writeFunc func(ctx context.Context, messages []T) error
-}
-
-func newWorker[T plugin.Message](writeFunc func(ctx context.Context, messages []T) error) *mixedBatchWorker[T] {
-	w := &mixedBatchWorker[T]{
-		writeFunc: writeFunc,
-		messages:  make([]T, 0, defaultBatchSize),
-		count:     0,
-		ch:        make(chan T),
-		wg:        &sync.WaitGroup{},
-	}
-	return w
-}
-
-func (w *mixedBatchWorker[T]) listen(ctx context.Context, ch <-chan T) chan chan bool {
-	flush := make(chan chan bool, 1)
-	w.wg.Add(1)
-	go func() {
-		defer w.wg.Done()
-		w.start(ctx, ch, flush)
-	}()
-	return flush
-}
-
-func (w *mixedBatchWorker[T]) start(ctx context.Context, ch <-chan T, flush chan chan bool) {
-	sizeBytes := int64(0)
-	messages := make([]T, 0)
-
-	for {
-		select {
-		case msg, ok := <-ch:
-			if !ok {
-				if len(messages) > 0 {
-					w.writeFunc(ctx, messages)
-				}
-				return
-			}
-			if uint64(len(messages)) == 1000 || sizeBytes+util.TotalRecordSize(r) > int64(1000) {
-				w.writeFunc(ctx, messages)
-				messages = make([]T, 0)
-				sizeBytes = 0
-			}
-			messages = append(messages, msg)
-			sizeBytes += util.TotalRecordSize(msg)
-		case <-time.After(w.batchTimeout):
-			if len(messages) > 0 {
-				w.writeFunc(ctx, messages)
-				messages = make([]T, 0)
-				sizeBytes = 0
-			}
-		case done := <-flush:
-			if len(messages) > 0 {
-				w.writeFunc(ctx, messages)
-				messages = make([]T, 0)
-				sizeBytes = 0
-			}
-			done <- true
-		}
-	}
-}
-
 func NewMixedBatchWriter(tables schema.Tables, client MixedBatchClient, opts ...MixedBatchWriterOption) (*MixedBatchWriter, error) {
 	c := &MixedBatchWriter{
 		tables:         tables,
 		client:         client,
-		workersLock:    &sync.Mutex{},
 		logger:         zerolog.Nop(),
 		batchTimeout:   defaultBatchTimeoutSeconds * time.Second,
 		batchSize:      defaultBatchSize,
 		batchSizeBytes: defaultBatchSizeBytes,
-
-		workerCreateTable: newWorker[plugin.MessageCreateTable](client.CreateTableBatch),
-		workerInsert:      newWorker[plugin.MessageInsert](client.InsertBatch),
-		workerDeleteStale: newWorker[plugin.MessageDeleteStale](client.DeleteStaleBatch),
 	}
 	for _, opt := range opts {
 		opt(c)
@@ -157,80 +78,7 @@ func NewMixedBatchWriter(tables schema.Tables, client MixedBatchClient, opts ...
 	return c, nil
 }
 
-// Write starts listening for messages on the msgChan channel and writes them to the client in batches.
-func (w *MixedBatchWriter) Write(ctx context.Context, msgChan <-chan plugin.Message) error {
-	w.workersLock.Lock()
-	flushCreateTable := w.workerCreateTable.listen(ctx, msgChan)
-	flushInsert := w.workerInsert.listen(ctx, msgChan)
-	flushDeleteStale := w.workerDeleteStale.listen(ctx, msgChan)
-	w.workersLock.Unlock()
-
-	done := make(chan bool)
-	for msg := range msgChan {
-		switch v := msg.(type) {
-		case plugin.MessageCreateTable:
-			w.workerCreateTable.ch <- v
-		case plugin.MessageInsert:
-			flushCreateTable <- done
-			<-done
-			flushDeleteStale <- done
-			<-done
-			w.workerInsert.ch <- v
-		case plugin.MessageDeleteStale:
-			flushCreateTable <- done
-			<-done
-			flushInsert <- done
-			<-done
-			w.workerDeleteStale.ch <- v
-		}
-	}
-
-	flushCreateTable <- done
-	<-done
-
-	flushInsert <- done
-	<-done
-
-	flushDeleteStale <- done
-	<-done
-
-	w.workersLock.Lock()
-	close(w.workerCreateTable.ch)
-	close(w.workerInsert.ch)
-	close(w.workerDeleteStale.ch)
-
-	w.workersLock.Unlock()
-	return nil
-}
-
-func (w *MixedBatchWriter) flush(ctx context.Context, messageID int, messages []plugin.Message) error {
-	var err error
-	switch messageID {
-	case msgTypeCreateTable:
-		msgs := make([]plugin.MessageCreateTable, len(messages))
-		for i := range messages {
-			msgs[i] = messages[i].(plugin.MessageCreateTable)
-		}
-		err = w.client.CreateTableBatch(ctx, msgs)
-	case msgTypeInsert:
-		// TODO: should we remove duplicates here?
-		w.writeInsert(ctx, messages)
-	case msgTypeDeleteStale:
-		w.writeDeleteStale(ctx, messages)
-	}
-	if err != nil {
-
-	}
-	start := time.Now()
-	batchSize := len(resources)
-	if err := w.client.WriteTableBatch(ctx, table, resources); err != nil {
-		w.logger.Err(err).Str("table", table.Name).Int("len", batchSize).Dur("duration", time.Since(start)).Msg("failed to write batch")
-	} else {
-		w.logger.Info().Str("table", table.Name).Int("len", batchSize).Dur("duration", time.Since(start)).Msg("batch written successfully")
-	}
-}
-
-func messageID(msg plugin.Message) int {
+func msgID(msg plugin.Message) int {
 	switch msg.(type) {
 	case plugin.MessageCreateTable:
 		return msgTypeCreateTable
@@ -238,7 +86,121 @@ func messageID(msg plugin.Message) int {
 		return msgTypeInsert
 	case plugin.MessageDeleteStale:
 		return msgTypeDeleteStale
-	default:
-		panic("unknown message type: " + reflect.TypeOf(msg).String())
 	}
+	panic("unknown message type")
+}
+
+// Write starts listening for messages on the msgChan channel and writes them to the client in batches.
+func (w *MixedBatchWriter) Write(ctx context.Context, msgChan <-chan plugin.Message) error {
+	createTable := &batchManager[plugin.MessageCreateTable]{
+		batch:     make([]plugin.MessageCreateTable, 0, w.batchSize),
+		writeFunc: w.client.CreateTableBatch,
+	}
+	insert := &insertBatchManager{
+		batch:             make([]plugin.MessageInsert, 0, w.batchSize),
+		writeFunc:         w.client.InsertBatch,
+		maxBatchSizeBytes: int64(w.batchSizeBytes),
+	}
+	deleteStale := &batchManager[plugin.MessageDeleteStale]{
+		batch:     make([]plugin.MessageDeleteStale, 0, w.batchSize),
+		writeFunc: w.client.DeleteStaleBatch,
+	}
+	flush := func(msgType int) error {
+		switch msgType {
+		case msgTypeCreateTable:
+			return createTable.flush(ctx)
+		case msgTypeInsert:
+			return insert.flush(ctx)
+		case msgTypeDeleteStale:
+			return deleteStale.flush(ctx)
+		default:
+			panic("unknown message type")
+		}
+	}
+	prevMsgType := -1
+	var err error
+	for msg := range msgChan {
+		msgType := msgID(msg)
+		if prevMsgType != -1 && prevMsgType != msgType {
+			if err := flush(prevMsgType); err != nil {
+				return err
+			}
+		}
+		prevMsgType = msgType
+		switch v := msg.(type) {
+		case plugin.MessageCreateTable:
+			err = createTable.append(ctx, v)
+		case plugin.MessageInsert:
+			err = insert.append(ctx, v)
+		case plugin.MessageDeleteStale:
+			err = deleteStale.append(ctx, v)
+		default:
+			panic("unknown message type")
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return flush(prevMsgType)
+}
+
+// generic batch manager for most message types
+type batchManager[T plugin.Message] struct {
+	batch     []T
+	writeFunc func(ctx context.Context, messages []T) error
+}
+
+func (m *batchManager[T]) append(ctx context.Context, msg T) error {
+	if len(m.batch) == cap(m.batch) {
+		if err := m.flush(ctx); err != nil {
+			return err
+		}
+	}
+	m.batch = append(m.batch, msg)
+	return nil
+}
+
+func (m *batchManager[T]) flush(ctx context.Context) error {
+	if len(m.batch) == 0 {
+		return nil
+	}
+
+	err := m.writeFunc(ctx, m.batch)
+	if err != nil {
+		return err
+	}
+	m.batch = m.batch[:0]
+	return nil
+}
+
+// special batch manager for insert messages that also keeps track of the total size of the batch
+type insertBatchManager struct {
+	batch             []plugin.MessageInsert
+	writeFunc         func(ctx context.Context, messages []plugin.MessageInsert) error
+	curBatchSizeBytes int64
+	maxBatchSizeBytes int64
+}
+
+func (m *insertBatchManager) append(ctx context.Context, msg plugin.MessageInsert) error {
+	if len(m.batch) == cap(m.batch) || m.curBatchSizeBytes+util.TotalRecordSize(msg.Record) > m.maxBatchSizeBytes {
+		if err := m.flush(ctx); err != nil {
+			return err
+		}
+	}
+	m.batch = append(m.batch, msg)
+	m.curBatchSizeBytes += util.TotalRecordSize(msg.Record)
+	return nil
+}
+
+func (m *insertBatchManager) flush(ctx context.Context) error {
+	if len(m.batch) == 0 {
+		return nil
+	}
+
+	err := m.writeFunc(ctx, m.batch)
+	if err != nil {
+		return err
+	}
+	m.batch = m.batch[:0]
+	return nil
 }
