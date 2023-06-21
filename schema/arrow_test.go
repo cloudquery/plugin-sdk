7f396cd2caa8f1d0e4f888bccf5e480536abfe92
Herman Schaaf <hermanschaaf@gmail.com>
2023-06-21 17:20:37 +0100

    Add test for NewRecordFromBytes

--- schema/arrow_test.go
+++ schema/arrow_test.go
@@ -1,9 +1,13 @@
 package schema
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/apache/arrow/go/v13/arrow"
+	"github.com/apache/arrow/go/v13/arrow/array"
+	"github.com/apache/arrow/go/v13/arrow/memory"
 )
 
 func TestSchemaEncode(t *testing.T) {
@@ -41,4 +45,59 @@ func TestSchemaEncode(t *testing.T) {
 			t.Fatalf("expected schema %d to be %v, got %v", i, schemas[i], decodedSchemas[i])
 		}
 	}
+}
+
+func TestRecordToBytesAndNewRecordFromBytes(t *testing.T) {
+	md := arrow.NewMetadata([]string{"key"}, []string{"value"})
+	schema := arrow.NewSchema(
+		[]arrow.Field{
+			{Name: "id", Type: arrow.PrimitiveTypes.Int64},
+			{Name: "name", Type: arrow.BinaryTypes.String},
+		},
+		&md,
+	)
+	bldr := array.NewRecordBuilder(memory.DefaultAllocator, schema)
+	defer bldr.Release()
+	bldr.Field(0).AppendValueFromString("1")
+	bldr.Field(1).AppendValueFromString("foo")
+	record := bldr.NewRecord()
+	b, err := RecordToBytes(record)
+	if err != nil {
+		t.Fatal(err)
+	}
+	decodedRecord, err := NewRecordFromBytes(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	numRows := record.NumRows()
+	if numRows != 1 {
+		t.Fatalf("expected 1 row, got %d", numRows)
+	}
+	if diff := RecordDiff(record, decodedRecord); diff != "" {
+		t.Fatalf("record differs from expected after NewRecordFromBytes: %v", diff)
+	}
+}
+
+func RecordDiff(l arrow.Record, r arrow.Record) string {
+	var sb strings.Builder
+	if l.NumCols() != r.NumCols() {
+		return fmt.Sprintf("different number of columns: %d vs %d", l.NumCols(), r.NumCols())
+	}
+	if l.NumRows() != r.NumRows() {
+		return fmt.Sprintf("different number of rows: %d vs %d", l.NumRows(), r.NumRows())
+	}
+	for i := 0; i < int(l.NumCols()); i++ {
+		edits, err := array.Diff(l.Column(i), r.Column(i))
+		if err != nil {
+			panic(fmt.Sprintf("left: %v, right: %v, error: %v", l.Column(i).DataType(), r.Column(i).DataType(), err))
+		}
+		diff := edits.UnifiedDiff(l.Column(i), r.Column(i))
+		if diff != "" {
+			sb.WriteString(l.Schema().Field(i).Name)
+			sb.WriteString(": ")
+			sb.WriteString(diff)
+			sb.WriteString("\n")
+		}
+	}
+	return sb.String()
 }
