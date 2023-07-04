57c7bc230c3ad3150f37b4f36b8e479b1c45c64f
Kemal <223029+disq@users.noreply.github.com>
2023-07-04 12:14:52 +0100

    fix(configtype): Add `Equal()` method to `Duration` (#1059)
    `cmp.Diff` doesn't like it otherwise.

--- configtype/duration_test.go
+++ configtype/duration_test.go
@@ -1,9 +1,12 @@
-package configtype
+package configtype_test
 
 import (
 	"encoding/json"
 	"testing"
 	"time"
+
+	"github.com/cloudquery/plugin-sdk/v4/configtype"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestDuration(t *testing.T) {
@@ -16,13 +19,36 @@ func TestDuration(t *testing.T) {
 		{"-50m30s", -50*time.Minute - 30*time.Second},
 	}
 	for _, tc := range cases {
-		var d Duration
+		var d configtype.Duration
 		err := json.Unmarshal([]byte(`"`+tc.give+`"`), &d)
 		if err != nil {
 			t.Fatalf("error calling Unmarshal(%q): %v", tc.give, err)
 		}
 		if d.Duration() != tc.want {
 			t.Errorf("Unmarshal(%q) = %v, want %v", tc.give, d.Duration(), tc.want)
+		}
+	}
+}
+
+func TestComparability(t *testing.T) {
+	cases := []struct {
+		give    configtype.Duration
+		compare configtype.Duration
+		equal   bool
+	}{
+		{configtype.NewDuration(0), configtype.NewDuration(0), true},
+		{configtype.NewDuration(0), configtype.NewDuration(1), false},
+	}
+	for _, tc := range cases {
+		if (tc.give == tc.compare) != tc.equal {
+			t.Errorf("comparing %v and %v should be %v", tc.give, tc.compare, tc.equal)
+		}
+
+		diff := cmp.Diff(tc.give, tc.compare)
+		if tc.equal && diff != "" {
+			t.Errorf("comparing %v and %v should be equal, but diff is %s", tc.give, tc.compare, diff)
+		} else if !tc.equal && diff == "" {
+			t.Errorf("comparing %v and %v should not be equal, but diff is empty", tc.give, tc.compare)
 		}
 	}
 }
