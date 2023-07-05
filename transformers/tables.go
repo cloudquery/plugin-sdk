6ecfb41a988df2d4554269d6f6dafbe15ef2aaa8
Kemal Hadimli <disq@users.noreply.github.com>
2023-07-05 11:06:39 +0100

    separate func

--- transformers/tables.go
+++ transformers/tables.go
@@ -14,20 +14,30 @@ func SetParents(tables schema.Tables, parent *schema.Table) {
 	}
 }
 
-// Apply transformations to tables
-func TransformTables(tables schema.Tables, extraTransformers ...schema.Transform) error {
+// TransformTables runs given Tables' transformers as defined in the table definitions, and recursively on the relations
+func TransformTables(tables schema.Tables) error {
 	for _, table := range tables {
 		if table.Transform != nil {
 			if err := table.Transform(table); err != nil {
 				return fmt.Errorf("failed to transform table %s: %w", table.Name, err)
 			}
 		}
+		if err := TransformTables(table.Relations); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ApplyTransformers applies the given transformers to the given Tables, and recursively on the relations
+func ApplyTransformers(tables schema.Tables, extraTransformers ...schema.Transform) error {
+	for _, table := range tables {
 		for _, tf := range extraTransformers {
 			if err := tf(table); err != nil {
 				return fmt.Errorf("failed to transform table %s: %w", table.Name, err)
 			}
 		}
-		if err := TransformTables(table.Relations, extraTransformers...); err != nil {
+		if err := ApplyTransformers(table.Relations, extraTransformers...); err != nil {
 			return err
 		}
 	}
