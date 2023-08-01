026c5b2a8b30612a58a8619f53607dae16bc8e7d
candiduslynx <candiduslynx@gmail.com>
2023-08-01 16:45:12 +0300

    process nulls for tested types, too (maps, lists, structs)

--- plugin/nulls.go
+++ plugin/nulls.go
@@ -6,41 +6,35 @@ import (
 	"github.com/apache/arrow/go/v13/arrow/memory"
 )
 
-func stripNullsFromLists(record arrow.Record) arrow.Record {
-	cols := record.Columns()
-	for c, col := range cols {
-		list, ok := col.(array.ListLike)
-		if !ok {
-			continue
-		}
-		if _, ok := list.(*array.Map); ok {
-			// maps also correspond to array.ListLike
-			continue
-		}
+func stripNullsFromLists(list array.ListLike) array.ListLike {
+	// TODO: handle Arrow maps separately if required
 
-		bldr := array.NewListBuilder(memory.DefaultAllocator, list.DataType().(arrow.ListLikeType).Elem())
-		for j := 0; j < list.Len(); j++ {
-			if list.IsNull(j) {
-				bldr.AppendNull()
+	if list.NullN() == 0 {
+		return list
+	}
+
+	bldr := array.NewBuilder(memory.DefaultAllocator, list.DataType()).(array.ListLikeBuilder)
+	for j := 0; j < list.Len(); j++ {
+		if list.IsNull(j) {
+			bldr.AppendNull()
+			continue
+		}
+		bldr.Append(true)
+		vBldr := bldr.ValueBuilder()
+		from, to := list.ValueOffsets(j)
+		slc := array.NewSlice(list.ListValues(), from, to)
+		for k := 0; k < int(to-from); k++ {
+			if slc.IsNull(k) {
 				continue
 			}
-			bldr.Append(true)
-			vBldr := bldr.ValueBuilder()
-			from, to := list.ValueOffsets(j)
-			slc := array.NewSlice(list.ListValues(), from, to)
-			for k := 0; k < int(to-from); k++ {
-				if slc.IsNull(k) {
-					continue
-				}
-				err := vBldr.AppendValueFromString(slc.ValueStr(k))
-				if err != nil {
-					panic(err)
-				}
+			err := vBldr.AppendValueFromString(slc.ValueStr(k))
+			if err != nil {
+				panic(err)
 			}
 		}
-		cols[c] = bldr.NewArray()
 	}
-	return array.NewRecord(record.Schema(), cols, record.NumRows())
+
+	return bldr.NewArray().(array.ListLike)
 }
 
 type AllowNullFunc func(arrow.DataType) bool
@@ -72,9 +66,76 @@ func (s *WriterTestSuite) replaceNullsByEmpty(record arrow.Record) arrow.Record 
 	return array.NewRecord(record.Schema(), cols, record.NumRows())
 }
 
-func (s *WriterTestSuite) handleNulls(record arrow.Record) arrow.Record {
-	if s.ignoreNullsInLists {
-		record = stripNullsFromLists(record)
+func (s *WriterTestSuite) replaceNullsByEmptyArray(arr arrow.Array) arrow.Array {
+	if s.allowNull == nil {
+		return arr
 	}
-	return s.replaceNullsByEmpty(record)
+
+	if !s.allowNull(arr.DataType()) && arr.NullN() > 0 {
+		builder := array.NewBuilder(memory.DefaultAllocator, arr.DataType())
+		for j := 0; j < arr.Len(); j++ {
+			if arr.IsNull(j) {
+				builder.AppendEmptyValue()
+				continue
+			}
+
+			if err := builder.AppendValueFromString(arr.ValueStr(j)); err != nil {
+				panic(err)
+			}
+		}
+
+		arr = builder.NewArray()
+	}
+
+	// we need to process the nested arrays, too
+	return s.replaceNullsByEmptyNestedArray(arr)
+}
+
+func (s *WriterTestSuite) replaceNullsByEmptyNestedArray(arr arrow.Array) arrow.Array {
+	if s.allowNull == nil {
+		return arr
+	}
+
+	switch arr := arr.(type) {
+	case array.ListLike: // TODO: handle Arrow maps separately if required
+		values := s.handleNullsArray(arr.ListValues())
+		return array.MakeFromData(
+			array.NewData(arr.DataType(), arr.Len(),
+				arr.Data().Buffers(),
+				[]arrow.ArrayData{values.Data()},
+				arr.NullN(), arr.Data().Offset(),
+			),
+		)
+	case *array.Struct:
+		children := make([]arrow.ArrayData, arr.NumField())
+		for i := 0; i < arr.NumField(); i++ {
+			children[i] = s.handleNullsArray(arr.Field(i)).Data()
+		}
+		return array.MakeFromData(
+			array.NewData(arr.DataType(), arr.Len(),
+				arr.Data().Buffers(),
+				children,
+				arr.NullN(), arr.Data().Offset(),
+			),
+		)
+	default:
+		return arr
+	}
+}
+
+func (s *WriterTestSuite) handleNulls(record arrow.Record) arrow.Record {
+	cols := record.Columns()
+	for c, col := range cols {
+		cols[c] = s.handleNullsArray(col)
+	}
+	return array.NewRecord(record.Schema(), cols, record.NumRows())
+
+}
+
+func (s *WriterTestSuite) handleNullsArray(arr arrow.Array) arrow.Array {
+	if arr, ok := arr.(array.ListLike); ok {
+		arr = stripNullsFromLists(arr) // TODO: handle Arrow maps separately if required
+	}
+
+	return s.replaceNullsByEmptyArray(arr)
 }
