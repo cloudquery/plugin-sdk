aeb02aa18b65d98df5e21e1212bdce361c13510e
candiduslynx <candiduslynx@gmail.com>
2023-08-30 14:58:51 +0300

    use unit & toTime

--- internal/memdb/memdb.go
+++ internal/memdb/memdb.go
@@ -216,8 +216,8 @@ func (c *client) deleteStale(_ context.Context, msg *message.WriteDeleteStale) {
 		syncColIndex := indices[0]
 
 		if row.Column(sourceColIndex).(*array.String).Value(0) == msg.SourceName {
-			toTime, _ := row.Column(syncColIndex).DataType().(*arrow.TimestampType).GetToTimeFunc()
-			rowSyncTime := toTime(row.Column(syncColIndex).(*array.Timestamp).Value(0)).UTC()
+			unit := row.Column(syncColIndex).DataType().(*arrow.TimestampType).Unit
+			rowSyncTime := row.Column(syncColIndex).(*array.Timestamp).Value(0).ToTime(unit).UTC()
 			if !rowSyncTime.Before(msg.SyncTime) {
 				filteredTable = append(filteredTable, c.memoryDB[tableName][i])
 			}
