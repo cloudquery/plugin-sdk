27c4e22fc4dd2db79b2e7ede35ecd1c135afe28c
Herman Schaaf <hermanschaaf@gmail.com>
2023-09-18 16:45:53 +0100

    Hash table names for random seed

--- scheduler/scheduler_shuffle.go
+++ scheduler/scheduler_shuffle.go
@@ -2,17 +2,21 @@ package scheduler
 
 import (
 	"context"
+	"hash/fnv"
 	"math/rand"
+	"strings"
 	"sync"
 
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 )
 
 func (s *syncClient) syncShuffle(ctx context.Context, resolvedResources chan<- *schema.Resource) {
-	// we have this because plugins can return sometimes clients in a random way which will cause
+	// We have this because plugins can return sometimes clients in a random way which will cause
 	// differences between this run and the next one.
 	preInitialisedClients := make([][]schema.ClientMeta, len(s.tables))
+	tableNames := make([]string, len(s.tables))
 	for i, table := range s.tables {
+		tableNames[i] = table.Name
 		clients := []schema.ClientMeta{s.client}
 		if table.Multiplex != nil {
 			clients = table.Multiplex(s.client)
@@ -23,11 +27,15 @@ func (s *syncClient) syncShuffle(ctx context.Context, resolvedResources chan<- *
 		s.metrics.initWithClients(table, clients)
 	}
 
-	// first interleave the tables like in round-robin
+	// First interleave the tables like in round-robin
 	tableClients := roundRobinInterleave(s.tables, preInitialisedClients)
 
-	// then shuffle the tableClients to randomize the order in which they are retrieved
-	shuffle(tableClients)
+	// Then shuffle the tableClients to randomize the order in which they are retrieved.
+	// We use a fixed seed so that runs with the same tables and clients perform similarly across syncs
+	// however, if the table order changes, the seed will change and the shuffle order will be different,
+	// so users have a little bit of control over the randomization.
+	seed := hashTableNames(tableNames)
+	shuffle(tableClients, seed)
 
 	var wg sync.WaitGroup
 	for _, tc := range tableClients {
@@ -42,8 +50,8 @@ func (s *syncClient) syncShuffle(ctx context.Context, resolvedResources chan<- *
 		go func() {
 			defer wg.Done()
 			defer s.scheduler.tableSems[0].Release(1)
-			// not checking for error here as nothing much to do.
-			// the error is logged and this happens when context is cancelled
+			// Not checking for error here as nothing much to do.
+			// the error is logged and this happens when context is cancelled.
 			// This currently uses the DFS algorithm to resolve the tables, but this
 			// may change in the future.
 			s.resolveTableDfs(ctx, table, cl, nil, resolvedResources, 1)
@@ -54,9 +62,14 @@ func (s *syncClient) syncShuffle(ctx context.Context, resolvedResources chan<- *
 	wg.Wait()
 }
 
-func shuffle(tableClients []tableClient) {
-	// use a fixed seed so that runs with the same tables and clients perform similarly across syncs
-	r := rand.New(rand.NewSource(99))
+func hashTableNames(tableNames []string) int64 {
+	h := fnv.New32a()
+	h.Write([]byte(strings.Join(tableNames, ",")))
+	return int64(h.Sum32())
+}
+
+func shuffle(tableClients []tableClient, seed int64) {
+	r := rand.New(rand.NewSource(seed))
 	r.Shuffle(len(tableClients), func(i, j int) {
 		tableClients[i], tableClients[j] = tableClients[j], tableClients[i]
 	})
