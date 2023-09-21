f6471e3ada92871e951026db9c1bf748a2e0b154
Herman Schaaf <hermanschaaf@gmail.com>
2023-09-21 13:29:01 +0100

    fix: Skip tables.json when packaging destinations (#1238)
    This adds a new `type` (`source` or `destination`) argument to the `package` command, and switches the ordering of arguments bit (since we're making a breaking change to it anyway). The `type` is written to package.json as well, and if it is `destination`, we don't create a `tables.json`.

--- serve/package_test.go
+++ serve/package_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestPluginPackage(t *testing.T) {
+func TestPluginPackage_Source(t *testing.T) {
 	_, filename, _, ok := runtime.Caller(0)
 	if !ok {
 		t.Fatal("failed to get current file path")
@@ -29,6 +29,90 @@ func TestPluginPackage(t *testing.T) {
 		memdb.NewMemDBClient,
 		plugin.WithBuildTargets([]plugin.BuildTarget{
 			{OS: plugin.GoOSLinux, Arch: plugin.GoArchAmd64},
+			{OS: plugin.GoOSWindows, Arch: plugin.GoArchAmd64},
+		}),
+	)
+	msg := `Test message
+with multiple lines and **markdown**`
+	testCases := []struct {
+		name    string
+		message string
+		wantErr bool
+	}{
+		{
+			name:    "inline message",
+			message: msg,
+		},
+		{
+			name:    "message from file",
+			message: "@testdata/message.txt",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			srv := Plugin(p)
+			cmd := srv.newCmdPluginRoot()
+			distDir := t.TempDir()
+			cmd.SetArgs([]string{"package", "--dist-dir", distDir, "-m", tc.message, "source", packageVersion, simplePluginPath})
+			err := cmd.Execute()
+			if tc.wantErr && err == nil {
+				t.Fatalf("expected error, got nil")
+			} else if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			files, err := os.ReadDir(distDir)
+			if err != nil {
+				t.Fatal(err)
+			}
+			expect := []string{
+				"docs",
+				"package.json",
+				"plugin-testPlugin-v1.2.3-linux-amd64.zip",
+				"plugin-testPlugin-v1.2.3-windows-amd64.zip",
+				"tables.json",
+			}
+			if diff := cmp.Diff(expect, fileNames(files)); diff != "" {
+				t.Fatalf("unexpected files in dist directory (-want +got):\n%s", diff)
+			}
+
+			expectPackage := PackageJSON{
+				SchemaVersion: 1,
+				Name:          "testPlugin",
+				Type:          "source",
+				Message:       msg,
+				Version:       "v1.2.3",
+				Protocols:     []int{3},
+				SupportedTargets: []TargetBuild{
+					{OS: plugin.GoOSLinux, Arch: plugin.GoArchAmd64, Path: "plugin-testPlugin-v1.2.3-linux-amd64.zip", Checksum: "sha256:" + sha256sum(filepath.Join(distDir, "plugin-testPlugin-v1.2.3-linux-amd64.zip"))},
+					{OS: plugin.GoOSWindows, Arch: plugin.GoArchAmd64, Path: "plugin-testPlugin-v1.2.3-windows-amd64.zip", Checksum: "sha256:" + sha256sum(filepath.Join(distDir, "plugin-testPlugin-v1.2.3-windows-amd64.zip"))},
+				},
+				PackageType: plugin.PackageTypeNative,
+			}
+			checkPackageJSONContents(t, filepath.Join(distDir, "package.json"), expectPackage)
+
+			expectDocs := []string{
+				"configuration.md",
+				"overview.md",
+			}
+			checkDocs(t, filepath.Join(distDir, "docs"), expectDocs)
+			checkTables(t, distDir)
+		})
+	}
+}
+
+func TestPluginPackage_Destination(t *testing.T) {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("failed to get current file path")
+	}
+	dir := filepath.Dir(filepath.Dir(filename))
+	simplePluginPath := filepath.Join(dir, "examples/simple_plugin")
+	packageVersion := "v1.2.3"
+	p := plugin.NewPlugin(
+		"testPlugin",
+		"development",
+		memdb.NewMemDBClient,
+		plugin.WithBuildTargets([]plugin.BuildTarget{
 			{OS: plugin.GoOSWindows, Arch: plugin.GoArchAmd64},
 			{OS: plugin.GoOSDarwin, Arch: plugin.GoArchAmd64},
 		}),
@@ -54,7 +138,7 @@ with multiple lines and **markdown**`
 			srv := Plugin(p)
 			cmd := srv.newCmdPluginRoot()
 			distDir := t.TempDir()
-			cmd.SetArgs([]string{"package", "--dist-dir", distDir, "-m", tc.message, simplePluginPath, packageVersion})
+			cmd.SetArgs([]string{"package", "--dist-dir", distDir, "-m", tc.message, "destination", packageVersion, simplePluginPath})
 			err := cmd.Execute()
 			if tc.wantErr && err == nil {
 				t.Fatalf("expected error, got nil")
@@ -69,9 +153,7 @@ with multiple lines and **markdown**`
 				"docs",
 				"package.json",
 				"plugin-testPlugin-v1.2.3-darwin-amd64.zip",
-				"plugin-testPlugin-v1.2.3-linux-amd64.zip",
 				"plugin-testPlugin-v1.2.3-windows-amd64.zip",
-				"tables.json",
 			}
 			if diff := cmp.Diff(expect, fileNames(files)); diff != "" {
 				t.Fatalf("unexpected files in dist directory (-want +got):\n%s", diff)
@@ -80,11 +162,11 @@ with multiple lines and **markdown**`
 			expectPackage := PackageJSON{
 				SchemaVersion: 1,
 				Name:          "testPlugin",
+				Type:          "destination",
 				Message:       msg,
 				Version:       "v1.2.3",
 				Protocols:     []int{3},
 				SupportedTargets: []TargetBuild{
-					{OS: plugin.GoOSLinux, Arch: plugin.GoArchAmd64, Path: "plugin-testPlugin-v1.2.3-linux-amd64.zip", Checksum: "sha256:" + sha256sum(filepath.Join(distDir, "plugin-testPlugin-v1.2.3-linux-amd64.zip"))},
 					{OS: plugin.GoOSWindows, Arch: plugin.GoArchAmd64, Path: "plugin-testPlugin-v1.2.3-windows-amd64.zip", Checksum: "sha256:" + sha256sum(filepath.Join(distDir, "plugin-testPlugin-v1.2.3-windows-amd64.zip"))},
 					{OS: plugin.GoOSDarwin, Arch: plugin.GoArchAmd64, Path: "plugin-testPlugin-v1.2.3-darwin-amd64.zip", Checksum: "sha256:" + sha256sum(filepath.Join(distDir, "plugin-testPlugin-v1.2.3-darwin-amd64.zip"))},
 				},
@@ -97,7 +179,6 @@ with multiple lines and **markdown**`
 				"overview.md",
 			}
 			checkDocs(t, filepath.Join(distDir, "docs"), expectDocs)
-			checkTables(t, distDir)
 		})
 	}
 }
