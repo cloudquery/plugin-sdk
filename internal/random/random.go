8d1195c7f7f8da3b729efe43d12979be2ea61511
candiduslynx <candiduslynx@gmail.com>
2023-09-29 07:31:50 +0300

    lint

--- internal/random/random.go
+++ internal/random/random.go
@@ -15,9 +15,9 @@ var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func randSeq(n int) string {
 	b := make([]rune, n)
-	rand.Seed(time.Now().UnixNano())
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = letters[rnd.Intn(len(letters))]
 	}
 	return string(b)
 }
