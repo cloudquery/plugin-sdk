2e00acd1f4cad3c3b71c2788ab4118b4200b8e81
Martin Norbury <martin.norbury@gmail.com>
2023-10-20 16:00:18 +0100

    Adding retry logic with `maxRetries` and `maxWaitTime`

--- premium/usage.go
+++ premium/usage.go
@@ -13,8 +13,10 @@ import (
 )
 
 const (
-	defaultBatchLimit = 1000
-	defaultDurationMS = 10000
+	defaultBatchLimit  = 1000
+	defaultDurationMS  = 10000
+	defaultMaxRetries  = 5
+	defaultMaxWaitTime = 60 * time.Second
 )
 
 type UsageClient interface {
@@ -28,15 +30,31 @@ type UsageClient interface {
 
 type UpdaterOptions func(updater *BatchUpdater)
 
+// WithBatchLimit sets the maximum number of rows to update in a single request
 func WithBatchLimit(batchLimit uint32) UpdaterOptions {
 	return func(updater *BatchUpdater) {
 		updater.batchLimit = batchLimit
 	}
 }
 
+// WithTickerDuration sets the duration between updates if the number of rows to update have not reached the batch limit
 func WithTickerDuration(durationms int) UpdaterOptions {
 	return func(updater *BatchUpdater) {
 		updater.tickerDuration = durationms
+	}
+}
+
+// WithMaxRetries sets the maximum number of retries to update the usage in case of an API error
+func WithMaxRetries(maxRetries int) UpdaterOptions {
+	return func(updater *BatchUpdater) {
+		updater.maxRetries = maxRetries
+	}
+}
+
+// WithMaxWaitTime sets the maximum time to wait before retrying a failed update
+func WithMaxWaitTime(maxWaitTime time.Duration) UpdaterOptions {
+	return func(updater *BatchUpdater) {
+		updater.maxWaitTime = maxWaitTime
 	}
 }
 
@@ -50,6 +68,8 @@ type BatchUpdater struct {
 
 	batchLimit     uint32
 	tickerDuration int
+	maxRetries     int
+	maxWaitTime    time.Duration
 	rowsToUpdate   atomic.Uint32
 	triggerUpdate  chan struct{}
 	done           chan struct{}
@@ -68,6 +88,8 @@ func NewUsageClient(ctx context.Context, apiClient *cqapi.ClientWithResponses, t
 
 		batchLimit:     defaultBatchLimit,
 		tickerDuration: defaultDurationMS,
+		maxRetries:     defaultMaxRetries,
+		maxWaitTime:    defaultMaxWaitTime,
 		triggerUpdate:  make(chan struct{}),
 		done:           make(chan struct{}),
 		wg:             &sync.WaitGroup{},
@@ -176,18 +198,34 @@ func (u *BatchUpdater) backgroundUpdater(ctx context.Context) {
 }
 
 func (u *BatchUpdater) updateUsageWithRetryAndBackoff(ctx context.Context, numberToUpdate uint32) error {
-	resp, err := u.apiClient.IncreaseTeamPluginUsageWithResponse(ctx, u.teamName, cqapi.IncreaseTeamPluginUsageJSONRequestBody{
-		RequestId:  uuid.New(),
-		PluginTeam: u.pluginTeam,
-		PluginKind: cqapi.PluginKind(u.pluginKind),
-		PluginName: u.pluginName,
-		Rows:       int(numberToUpdate),
-	})
-	if err != nil {
-		return fmt.Errorf("failed to update usage: %w", err)
+	var retryDelay time.Duration
+
+	for retry := 0; retry < u.maxRetries; retry++ {
+		startTime := time.Now()
+
+		resp, err := u.apiClient.IncreaseTeamPluginUsageWithResponse(ctx, u.teamName, cqapi.IncreaseTeamPluginUsageJSONRequestBody{
+			RequestId:  uuid.New(),
+			PluginTeam: u.pluginTeam,
+			PluginKind: cqapi.PluginKind(u.pluginKind),
+			PluginName: u.pluginName,
+			Rows:       int(numberToUpdate),
+		})
+		if err != nil {
+			return fmt.Errorf("failed to update usage: %w", err)
+		}
+		if resp.StatusCode() == http.StatusOK {
+			return nil
+		}
+
+		retryDelay = time.Duration(1<<retry) * time.Second
+		if retryDelay > u.maxWaitTime {
+			retryDelay = u.maxWaitTime
+		}
+
+		sleepDuration := retryDelay - time.Since(startTime)
+		if sleepDuration > 0 {
+			time.Sleep(sleepDuration)
+		}
 	}
-	if resp.StatusCode() != http.StatusOK {
-		return fmt.Errorf("failed to update usage: %s", resp.Status())
-	}
-	return nil
+	return fmt.Errorf("failed to update usage: max retries exceeded")
 }
