21d6090d2e0f1a97e7e8e02807516243516703ec
candiduslynx <candiduslynx@gmail.com>
2023-11-14 12:33:37 +0200

    remove `MakeTablePaid`

--- premium/tables.go
+++ premium/tables.go
@@ -15,14 +15,8 @@ func ContainsPaidTables(tables schema.Tables) bool {
 // MakeAllTablesPaid sets all tables to paid (including relations)
 func MakeAllTablesPaid(tables schema.Tables) schema.Tables {
 	for _, table := range tables {
-		MakeTablePaid(table)
+		table.IsPaid = true
+		MakeAllTablesPaid(table.Relations)
 	}
 	return tables
-}
-
-// MakeTablePaid sets the table to paid (including relations)
-func MakeTablePaid(table *schema.Table) *schema.Table {
-	table.IsPaid = true
-	MakeAllTablesPaid(table.Relations)
-	return table
 }
