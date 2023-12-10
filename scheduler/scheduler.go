5d4a124c38c02bbe48117a038af05b59c97b7e63
bbernays <ben@bernays.me>
2023-12-10 17:37:20 -0600

    Update scheduler.go

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"runtime/debug"
+	"sync"
 	"sync/atomic"
 	"time"
 
@@ -65,6 +66,12 @@ func WithStrategy(strategy Strategy) Option {
 	}
 }
 
+func WithSingleTableMaxConcurrency(concurrency int) Option {
+	return func(s *Scheduler) {
+		s.singleTableMaxConcurrency = concurrency
+	}
+}
+
 type SyncOption func(*syncClient)
 
 func WithSyncDeterministicCQID(deterministicCQID bool) SyncOption {
@@ -86,8 +93,10 @@ type Scheduler struct {
 	// tableSem is a semaphore that limits the number of concurrent tables being fetched
 	tableSems []*semaphore.Weighted
 	// Logger to call, this logger is passed to the serve.Serve Client, if not defined Serve will create one instead.
-	logger      zerolog.Logger
-	concurrency int
+	logger                    zerolog.Logger
+	concurrency               int
+	singleTableConcurrency    sync.Map
+	singleTableMaxConcurrency int
 }
 
 type syncClient struct {
