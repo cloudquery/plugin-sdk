253f4d25038d6236773d9ed065ace2be96f35b62
bbernays <ben@bernays.me>
2024-01-05 16:49:16 -0600

    Update scheduler.go

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -191,7 +191,11 @@ func (s *Scheduler) Sync(ctx context.Context, client schema.ClientMeta, tables s
 	for _, table := range tables.FlattenTables() {
 		if syncClient.deterministicCQID {
 			for i, c := range table.Columns {
-				if c.PrimaryKey && c.Name != schema.CqIDColumn.Name {
+				if c.Name == schema.CqIDColumn.Name {
+					// CQ_ID should be PK and not have a Unique Clause on it
+					table.Columns[i].PrimaryKey = true
+					table.Columns[i].Unique = false
+				} else if c.PrimaryKey {
 					table.Columns[i].PrimaryKey = false
 				}
 			}
