d66fd9c6a1b4b6d12a89f8871c352e476c6e103e
Kemal Hadimli <disq@users.noreply.github.com>
2024-03-05 12:20:00 +0000

    chore: Batch writer benchmarks

---
+++ writers/writers_test.go
@@ -0,0 +1,209 @@
+package writers_test
+
+import (
+	"context"
+	"math/rand"
+	"runtime"
+	"sort"
+	"strconv"
+	"testing"
+
+	"github.com/apache/arrow/go/v15/arrow"
+	"github.com/apache/arrow/go/v15/arrow/array"
+	"github.com/apache/arrow/go/v15/arrow/memory"
+	"github.com/cloudquery/plugin-sdk/v4/message"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/cloudquery/plugin-sdk/v4/writers"
+	"github.com/cloudquery/plugin-sdk/v4/writers/batchwriter"
+	"github.com/cloudquery/plugin-sdk/v4/writers/mixedbatchwriter"
+	"github.com/cloudquery/plugin-sdk/v4/writers/streamingbatchwriter"
+	"golang.org/x/exp/maps"
+)
+
+type bCase struct {
+	name string
+	wr   writers.Writer
+	rec  func() arrow.Record
+}
+
+func BenchmarkWriterMemory(b *testing.B) {
+	batchwriterOpts := map[string][]batchwriter.Option{
+		"defaults":           nil,
+		"batch10k/bytes100M": {batchwriter.WithBatchSizeBytes(100000000), batchwriter.WithBatchSize(10000)},
+	}
+	mixedbatchwriterOpts := map[string][]mixedbatchwriter.Option{
+		"defaults":           nil,
+		"batch10k/bytes100M": {mixedbatchwriter.WithBatchSizeBytes(100000000), mixedbatchwriter.WithBatchSize(10000)},
+	}
+	streamingbatchwriterOpts := map[string][]streamingbatchwriter.Option{
+		"defaults":  nil,
+		"bytes100M": {streamingbatchwriter.WithBatchSizeBytes(100000000)},
+	}
+
+	var bCases []bCase
+	bCases = append(bCases, writerMatrix[*batchwriter.BatchWriter, batchwriter.Client, batchwriter.Option]("BatchWriter", batchwriter.New, &batchwriterClient{}, makeRecord, batchwriterOpts)...)
+	bCases = append(bCases, writerMatrix[*batchwriter.BatchWriter, batchwriter.Client, batchwriter.Option]("BatchWriter: wide", batchwriter.New, &batchwriterClient{}, makeWideRecord, batchwriterOpts)...)
+	bCases = append(bCases, writerMatrix[*mixedbatchwriter.MixedBatchWriter, mixedbatchwriter.Client, mixedbatchwriter.Option]("MixedBatchWriter", mixedbatchwriter.New, &mixedbatchwriterClient{}, makeRecord, mixedbatchwriterOpts)...)
+	bCases = append(bCases, writerMatrix[*mixedbatchwriter.MixedBatchWriter, mixedbatchwriter.Client, mixedbatchwriter.Option]("MixedBatchWriter: wide", mixedbatchwriter.New, &mixedbatchwriterClient{}, makeWideRecord, mixedbatchwriterOpts)...)
+	bCases = append(bCases, writerMatrix[*streamingbatchwriter.StreamingBatchWriter, streamingbatchwriter.Client, streamingbatchwriter.Option]("StreamingBatchWriter", streamingbatchwriter.New, &streamingbatchwriterClient{}, makeRecord, streamingbatchwriterOpts)...)
+	bCases = append(bCases, writerMatrix[*streamingbatchwriter.StreamingBatchWriter, streamingbatchwriter.Client, streamingbatchwriter.Option]("StreamingBatchWriter: wide", streamingbatchwriter.New, &streamingbatchwriterClient{}, makeWideRecord, streamingbatchwriterOpts)...)
+
+	_ = batchwriterOpts
+	_ = streamingbatchwriterOpts
+
+	for _, c := range bCases {
+		c := c
+		b.Run(c.name, func(b *testing.B) {
+			var (
+				mStart runtime.MemStats
+				mEnd   runtime.MemStats
+			)
+
+			ch := make(chan message.WriteMessage)
+			errCh := make(chan error)
+			go func() {
+				defer close(errCh)
+				errCh <- c.wr.Write(context.Background(), ch)
+			}()
+
+			runtime.ReadMemStats(&mStart)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				rec := c.rec()
+				ch <- &message.WriteInsert{
+					Record: rec,
+				}
+			}
+			close(ch)
+			err := <-errCh
+
+			b.StopTimer()
+
+			if err != nil {
+				b.Fatal(err)
+			}
+
+			runtime.ReadMemStats(&mEnd)
+
+			allocatedBytes := mEnd.Alloc - mStart.Alloc
+			b.ReportMetric(float64(allocatedBytes)/float64(b.N), "bytes/op")
+		})
+	}
+}
+
+func makeRecord() func() arrow.Record {
+	table := &schema.Table{
+		Name: "test_table",
+		Columns: schema.ColumnList{
+			{
+				Name: "col1",
+				Type: arrow.BinaryTypes.String,
+			},
+		},
+	}
+	sc := table.ToArrowSchema()
+
+	return func() arrow.Record {
+		bldr := array.NewRecordBuilder(memory.DefaultAllocator, sc)
+		bldr.Field(0).(*array.StringBuilder).Append("test")
+		return bldr.NewRecord()
+	}
+}
+
+func makeWideRecord() func() arrow.Record {
+	table := &schema.Table{
+		Name: "test_wide_table",
+		Columns: schema.ColumnList{
+			{
+				Name: "col1",
+				Type: arrow.BinaryTypes.String,
+			},
+		},
+	}
+
+	const numWideCols = 200
+	randVals := make([]int64, numWideCols)
+	for i := 0; i < numWideCols; i++ {
+		table.Columns = append(table.Columns, schema.Column{
+			Name: "wide_col" + strconv.Itoa(i),
+			Type: arrow.PrimitiveTypes.Int64,
+		})
+		randVals[i] = rand.Int63()
+	}
+	sc := table.ToArrowSchema()
+
+	return func() arrow.Record {
+		bldr := array.NewRecordBuilder(memory.DefaultAllocator, sc)
+		bldr.Field(0).(*array.StringBuilder).Append("test")
+		for i := 0; i < numWideCols; i++ {
+			bldr.Field(i + 1).(*array.Int64Builder).Append(randVals[i])
+		}
+		return bldr.NewRecord()
+	}
+}
+
+func writerMatrix[T writers.Writer, C any, O ~func(T)](prefix string, constructor func(C, ...O) (T, error), client C, recordMaker func() func() arrow.Record, optsMatrix map[string][]O) []bCase {
+	var bCases []bCase
+
+	k := maps.Keys(optsMatrix)
+	sort.Strings(k)
+
+	for _, name := range k {
+		opts := optsMatrix[name]
+		wr, err := constructor(client, opts...)
+		if err != nil {
+			panic(err)
+		}
+		bCases = append(bCases, bCase{
+			name: prefix + ": " + name,
+			wr:   wr,
+			rec:  recordMaker(),
+		})
+	}
+	return bCases
+}
+
+type mixedbatchwriterClient struct {
+	mixedbatchwriter.IgnoreMigrateTableBatch
+	mixedbatchwriter.UnimplementedDeleteStaleBatch
+	mixedbatchwriter.UnimplementedDeleteRecordsBatch
+}
+
+func (mixedbatchwriterClient) InsertBatch(_ context.Context, msgs message.WriteInserts) error {
+	for _, m := range msgs {
+		m.Record.Release()
+	}
+	return nil
+}
+
+var _ mixedbatchwriter.Client = (*mixedbatchwriterClient)(nil)
+
+type batchwriterClient struct {
+	batchwriter.IgnoreMigrateTables
+	batchwriter.UnimplementedDeleteStale
+	batchwriter.UnimplementedDeleteRecord
+}
+
+func (batchwriterClient) WriteTableBatch(_ context.Context, _ string, msgs message.WriteInserts) error {
+	for _, m := range msgs {
+		m.Record.Release()
+	}
+	return nil
+}
+
+var _ batchwriter.Client = (*batchwriterClient)(nil)
+
+type streamingbatchwriterClient struct {
+	streamingbatchwriter.IgnoreMigrateTable
+	streamingbatchwriter.UnimplementedDeleteStale
+	streamingbatchwriter.UnimplementedDeleteRecords
+}
+
+func (streamingbatchwriterClient) WriteTable(_ context.Context, ch <-chan *message.WriteInsert) error {
+	for m := range ch {
+		m.Record.Release()
+	}
+	return nil
+}
+
+var _ streamingbatchwriter.Client = (*streamingbatchwriterClient)(nil)
