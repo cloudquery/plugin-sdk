d8fde8c87a82617dc180c273cfd9cb70dcddfe13
Kemal <223029+disq@users.noreply.github.com>
2024-04-08 13:48:48 +0100

    feat: Support arbitrary map values for structs (scalar) (#1611)
    Seems to work, although you still need to transform a struct's underlying types (if necessary)
    
    For example in BQ source, passing `map[string]bigquery.Value` now works but since we still have to transform some of the datatypes, one still needs to transform each sub item separately and update map values.

--- scalar/scalar.go
+++ scalar/scalar.go
@@ -2,6 +2,7 @@ package scalar
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/apache/arrow/go/v15/arrow"
 	"github.com/apache/arrow/go/v15/arrow/array"
@@ -208,7 +209,23 @@ func AppendToBuilder(bldr array.Builder, s Scalar) {
 		sb.Append(true)
 
 		v := s.(*Struct).Value
-		m := v.(map[string]any)
+		m, ok := v.(map[string]any)
+		if !ok {
+			reflectedMap := reflect.ValueOf(v)
+			if reflectedMap.Kind() != reflect.Map {
+				panic(fmt.Sprintf("expected map[string]any, got %T", v))
+			}
+
+			m = make(map[string]any, reflectedMap.Len())
+			for _, key := range reflectedMap.MapKeys() {
+				if key.Kind() != reflect.String {
+					panic(fmt.Sprintf("expected map[string]any, got %T", v))
+				}
+				value := reflectedMap.MapIndex(key)
+				m[key.String()] = value.Interface()
+			}
+		}
+
 		names := make(map[string]struct{}, len(m))
 		for k := range m {
 			names[k] = struct{}{}
