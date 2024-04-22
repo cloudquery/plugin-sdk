cloudquery/plugin-sdk

commit f565da8961db0b9f88efcdaa6f083faa789de324

fix: MixedBatchWriter should nil the slice instead of zeroing (#1553)
Helps with memory.

Benched with https://github.com/cloudquery/plugin-sdk/pull/1552, old:

```
BenchmarkWriterMemory/MixedBatchWriter_batch10k_bytes100M
BenchmarkWriterMemory/MixedBatchWriter_batch10k_bytes100M-10    	     826	   1445595 ns/op	      1393 bytes/op
BenchmarkWriterMemory/MixedBatchWriter_defaults
BenchmarkWriterMemory/MixedBatchWriter_defaults-10              	     670	   1504799 ns/op	      1393 bytes/op
BenchmarkWriterMemory/MixedBatchWriter_wide_batch10k_bytes100M
BenchmarkWriterMemory/MixedBatchWriter_wide_batch10k_bytes100M-10         	       8	 128966609 ns/op	    172785 bytes/op
BenchmarkWriterMemory/MixedBatchWriter_wide_defaults
BenchmarkWriterMemory/MixedBatchWriter_wide_defaults-10                   	       9	 118630315 ns/op	     60355 bytes/op
```

new:
```
BenchmarkWriterMemory/MixedBatchWriter_batch10k_bytes100M
BenchmarkWriterMemory/MixedBatchWriter_batch10k_bytes100M-10    	     690	   1476479 ns/op	      1401 bytes/op
BenchmarkWriterMemory/MixedBatchWriter_defaults
BenchmarkWriterMemory/MixedBatchWriter_defaults-10              	     687	   1539707 ns/op	      1401 bytes/op
BenchmarkWriterMemory/MixedBatchWriter_wide_batch10k_bytes100M
BenchmarkWriterMemory/MixedBatchWriter_wide_batch10k_bytes100M-10         	       8	 129154755 ns/op	    173592 bytes/op
BenchmarkWriterMemory/MixedBatchWriter_wide_defaults
BenchmarkWriterMemory/MixedBatchWriter_wide_defaults-10                   	       9	 111416773 ns/op	     44588 bytes/op
```

writers/mixedbatchwriter/mixedbatchwriter.go
@@ -192,7 +192,7 @@ func (m *batchManager[A, T]) flush(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	m.batch = m.batch[:0]
+	m.batch = nil
 	return nil
 }
 
@@ -229,7 +229,7 @@ func (m *insertBatchManager) flush(ctx context.Context) error {
 	}
 	m.logger.Debug().Int("len", batchSize).Dur("duration", time.Since(start)).Msg("batch written successfully")
 
-	m.batch = m.batch[:0]
+	m.batch = nil
 	m.curBatchSizeBytes = 0
 	return nil
 }

commit 07945ac593e2965b8f80ed3b4ed8724ac610f2bd

fix: Use `clear` for mixed batch writer (#1645)
Fixes the issue with `cap` used on `nil` slice introduced in https://github.com/cloudquery/plugin-sdk/pull/1553

writers/mixedbatchwriter/mixedbatchwriter.go
@@ -192,7 +192,8 @@ func (m *batchManager[A, T]) flush(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	m.batch = nil
+	clear(m.batch) // GC can work
+	m.batch = m.batch[:0]
 	return nil
 }
 
@@ -229,7 +230,8 @@ func (m *insertBatchManager) flush(ctx context.Context) error {
 	}
 	m.logger.Debug().Int("len", batchSize).Dur("duration", time.Since(start)).Msg("batch written successfully")
 
-	m.batch = nil
+	clear(m.batch) // GC can work
+	m.batch = m.batch[:0]
 	m.curBatchSizeBytes = 0
 	return nil
 }