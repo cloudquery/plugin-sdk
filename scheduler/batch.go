470c5763221b545bce693e43c4efabaeae9a568c
candiduslynx <candiduslynx@gmail.com>
2024-05-11 08:33:36 +0300

    v16

--- scheduler/batch.go
+++ scheduler/batch.go
@@ -5,8 +5,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/apache/arrow/go/v15/arrow/array"
-	"github.com/apache/arrow/go/v15/arrow/memory"
+	"github.com/apache/arrow/go/v16/arrow/array"
+	"github.com/apache/arrow/go/v16/arrow/memory"
 	"github.com/cloudquery/plugin-sdk/v4/message"
 	"github.com/cloudquery/plugin-sdk/v4/scalar"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
