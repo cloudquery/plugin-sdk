e27a4bc8b95b3bf068ce289f9b32818b82949668
candiduslynx <candiduslynx@gmail.com>
2024-05-16 15:10:28 +0300

    less flappy test

--- scalar/date32_test.go
+++ scalar/date32_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/apache/arrow/go/v16/arrow"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDate32Set(t *testing.T) {
@@ -12,22 +13,19 @@ func TestDate32Set(t *testing.T) {
 		source any
 		result Date32
 	}{
-		{source: time.Date(1900, 1, 1, 0, 0, 0, 0, time.Local), result: Date32{Value: arrow.Date32FromTime(time.Date(1900, 1, 1, 0, 0, 0, 0, time.Local)), Valid: true}},
-		{source: time.Date(1999, 12, 31, 12, 59, 59, 0, time.Local), result: Date32{Value: arrow.Date32FromTime(time.Date(1999, 12, 31, 0, 0, 0, 0, time.Local)), Valid: true}},
+		{source: time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC),
+			result: Date32{Value: arrow.Date32FromTime(time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)), Valid: true}},
+		{source: time.Date(1999, 12, 31, 12, 59, 59, 0, time.UTC),
+			result: Date32{Value: arrow.Date32FromTime(time.Date(1999, 12, 31, 0, 0, 0, 0, time.UTC)), Valid: true}},
 		{source: "2150-10-15", result: Date32{Value: arrow.Date32FromTime(time.Date(2150, 10, 15, 0, 0, 0, 0, time.UTC)), Valid: true}},
 		{source: "", result: Date32{}},
 	}
 
 	for i, tt := range successfulTests {
-		var r Date32
-		err := r.Set(tt.source)
-		if err != nil {
-			t.Errorf("%d: %v", i, err)
-			continue
-		}
-
-		if !r.Equal(&tt.result) {
-			t.Errorf("%d: %v != %v", i, r, tt.result)
-		}
+		t.Run(tt.result.String(), func(t *testing.T) {
+			var r Date32
+			assert.NoError(t, r.Set(tt.source))
+			assert.Truef(t, r.Equal(&tt.result), "%d: %v != %v", i, r, tt.result)
+		})
 	}
 }
