5a59978f937fd7a8371425050235f68c2aa5706d
candiduslynx <candiduslynx@gmail.com>
2024-06-06 18:29:36 +0300

    https://github.com/cloudquery/plugin-sdk/pull/1664

--- docs/markdown.go
+++ docs/markdown.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"text/template"
 
+	"github.com/apache/arrow/go/v16/arrow"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
 )
 
@@ -57,7 +60,8 @@ func (g *Generator) renderAllTables(dir string, t *schema.Table) error {
 
 func (g *Generator) renderTable(dir string, table *schema.Table) error {
 	t := template.New("").Funcs(map[string]any{
-		"title": g.titleTransformer,
+		"title":           g.titleTransformer,
+		"colTypeWithCode": colTypeWithCode,
 	})
 	t, err := t.New("table.md.go.tpl").ParseFS(templatesFS, "templates/table.md.go.tpl")
 	if err != nil {
@@ -93,4 +97,127 @@ func indentToDepth(table *schema.Table) string {
 		t = t.Parent
 	}
 	return s
+}
+
+func colTypeWithCode(dt arrow.DataType) string {
+	typ := colType(dt, 0)
+	if strings.Contains(typ, "<br>") {
+		// pre block
+		return "<pre>" + typ + "</pre>"
+	}
+	return "`" + typ + "`"
+}
+
+func colType(dt arrow.DataType, level int) string {
+	nested, ok := dt.(arrow.NestedType)
+	if !ok {
+		return dt.String()
+	}
+	switch nested := nested.(type) {
+	case *arrow.StructType:
+		return structType(nested, level)
+	case *arrow.MapType:
+		return mapType(nested, level)
+	case arrow.ListLikeType:
+		return listLikeType(nested, level)
+	default:
+		return dt.String()
+	}
+}
+
+func structType(dt *arrow.StructType, level int) string {
+	if !needsMultiline(dt) {
+		return simpleStruct(dt)
+	}
+
+	var buf strings.Builder
+	buf.WriteString("<br>") // for starting elems with a newline
+	pfx := strings.Repeat("&nbsp;", level+1)
+	for i, field := range dt.Fields() {
+		if i > 0 {
+			buf.WriteString(",<br>")
+		}
+		buf.WriteString(pfx)
+		buf.WriteString(field.Name)
+		buf.WriteString(": ")
+		buf.WriteString(colType(field.Type, level+1))
+		if field.Nullable {
+			buf.WriteString("?")
+		}
+	}
+	return "struct<" + buf.String() + "<br>" + strings.Repeat("&nbsp;", level) + ">"
+}
+
+func simpleStruct(dt *arrow.StructType) string {
+	field := dt.Field(0)
+	res := "struct<" + field.Name + ": " + field.Type.String()
+	if field.Nullable {
+		res += "?"
+	}
+	return res + ">"
+}
+
+func mapType(dt *arrow.MapType, level int) string {
+	keys := colType(dt.KeyType(), level)
+	items := colType(dt.ItemType(), level) + "?" // always have nullable values in map
+	return "map<" + keys + ", " + items + ">"
+}
+
+func listLikeType(dt arrow.ListLikeType, level int) string {
+	elemField := dt.ElemField()
+	nested := needsMultiline(dt)
+	if _, nested = elemField.Type.(arrow.NestedType); nested {
+		level += 1 // nested types will require additional handling
+	}
+
+	var elems string
+	var pfx, sfx string
+	if nested {
+		pfx = "<br>" + strings.Repeat("&nbsp;", level)
+		sfx = "<br>"
+		if level > 0 {
+			sfx += strings.Repeat("&nbsp;", level-1)
+		}
+	}
+	elems = "<" + pfx + colType(elemField.Type, level)
+	if dt.ElemField().Nullable {
+		elems += "?"
+	}
+	elems += sfx + ">"
+	switch dt := dt.(type) {
+	case *arrow.ListType:
+		return "list" + elems
+	case *arrow.LargeListType:
+		return "large_list" + elems
+	case *arrow.FixedSizeListType:
+		return "fixed_size_list" + elems + "[" + strconv.FormatInt(int64(dt.Len()), 10) + "]"
+	case *arrow.ListViewType:
+		return "list_view" + elems
+	default:
+		return dt.Name() + elems
+	}
+}
+
+func needsMultiline(dt arrow.DataType) bool {
+	nested, ok := dt.(arrow.NestedType)
+	if !ok {
+		return false
+	}
+	switch nested := nested.(type) {
+	case *arrow.MapType:
+		return needsMultiline(nested.ItemType()) // keys are presumed to be simple
+	case arrow.ListLikeType:
+		return needsMultiline(nested.Elem())
+	case *arrow.StructType:
+		switch nested.NumFields() {
+		case 0:
+			return false
+		case 1:
+			return needsMultiline(nested.Field(0).Type)
+		default:
+			return true
+		}
+	default:
+		return false
+	}
 }
