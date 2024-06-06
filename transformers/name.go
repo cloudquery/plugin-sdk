7b9d6cc7fbf2a47c25af5e07a24ba09108c1cceb
Alex Shcherbakov <candiduslynx@users.noreply.github.com>
2024-06-06 17:44:45 +0300

    refactor: Split `transformers/struct.go` (#1725)

---
+++ transformers/name.go
@@ -0,0 +1,29 @@
+package transformers
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/cloudquery/plugin-sdk/v4/caser"
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+)
+
+type NameTransformer func(reflect.StructField) (string, error)
+
+var defaultCaser = caser.New()
+
+func DefaultNameTransformer(field reflect.StructField) (string, error) {
+	name := field.Name
+	if jsonTag := strings.Split(field.Tag.Get("json"), ",")[0]; len(jsonTag) > 0 {
+		// return empty string if the field is not related api response
+		if jsonTag == "-" {
+			return "", nil
+		}
+		if nameFromJSONTag := defaultCaser.ToSnake(jsonTag); schema.ValidColumnName(nameFromJSONTag) {
+			return nameFromJSONTag, nil
+		}
+	}
+	return defaultCaser.ToSnake(name), nil
+}
+
+var _ NameTransformer = DefaultNameTransformer
--- transformers/struct.go
+++ transformers/struct.go
@@ -2,16 +2,10 @@ package transformers
 
 import (
 	"fmt"
-	"net"
 	"reflect"
 	"slices"
-	"strings"
-	"time"
 
-	"github.com/apache/arrow/go/v16/arrow"
-	"github.com/cloudquery/plugin-sdk/v4/caser"
 	"github.com/cloudquery/plugin-sdk/v4/schema"
-	"github.com/cloudquery/plugin-sdk/v4/types"
 	"github.com/thoas/go-funk"
 )
 
@@ -30,24 +24,6 @@ type structTransformer struct {
 	pkComponentFieldsFound        []string
 }
 
-type NameTransformer func(reflect.StructField) (string, error)
-
-type TypeTransformer func(reflect.StructField) (arrow.DataType, error)
-
-type ResolverTransformer func(field reflect.StructField, path string) schema.ColumnResolver
-
-func DefaultResolverTransformer(_ reflect.StructField, path string) schema.ColumnResolver {
-	return schema.PathResolver(path)
-}
-
-type IgnoreInTestsTransformer func(field reflect.StructField) bool
-
-func DefaultIgnoreInTestsTransformer(_ reflect.StructField) bool {
-	return false
-}
-
-type StructTransformerOption func(*structTransformer)
-
 func isFieldStruct(reflectType reflect.Type) bool {
 	switch reflectType.Kind() {
 	case reflect.Struct:
@@ -59,120 +35,14 @@ func isFieldStruct(reflectType reflect.Type) bool {
 	}
 }
 
-// WithUnwrapAllEmbeddedStructs instructs codegen to unwrap all embedded fields (1 level deep only)
-func WithUnwrapAllEmbeddedStructs() StructTransformerOption {
-	return func(t *structTransformer) {
-		t.unwrapAllEmbeddedStructFields = true
-	}
-}
-
-// WithUnwrapStructFields allows to unwrap specific struct fields (1 level deep only)
-func WithUnwrapStructFields(fields ...string) StructTransformerOption {
-	return func(t *structTransformer) {
-		t.structFieldsToUnwrap = fields
-	}
-}
-
-// WithSkipFields allows to specify what struct fields should be skipped.
-func WithSkipFields(fields ...string) StructTransformerOption {
-	return func(t *structTransformer) {
-		t.skipFields = fields
-	}
-}
-
-// WithNameTransformer overrides how column name will be determined.
-// DefaultNameTransformer is used as the default.
-func WithNameTransformer(transformer NameTransformer) StructTransformerOption {
-	return func(t *structTransformer) {
-		t.nameTransformer = transformer
-	}
-}
-
-// WithTypeTransformer overrides how column type will be determined.
-// DefaultTypeTransformer is used as the default.
-func WithTypeTransformer(transformer TypeTransformer) StructTransformerOption {
-	return func(t *structTransformer) {
-		t.typeTransformer = transformer
-	}
-}
-
-// WithResolverTransformer overrides how column resolver will be determined.
-// DefaultResolverTransformer is used as the default.
-func WithResolverTransformer(transformer ResolverTransformer) StructTransformerOption {
-	return func(t *structTransformer) {
-		t.resolverTransformer = transformer
-	}
-}
-
-// WithIgnoreInTestsTransformer overrides how column ignoreInTests will be determined.
-// DefaultIgnoreInTestsTransformer is used as the default.
-func WithIgnoreInTestsTransformer(transformer IgnoreInTestsTransformer) StructTransformerOption {
-	return func(t *structTransformer) {
-		t.ignoreInTestsTransformer = transformer
-	}
-}
-
-// WithPrimaryKeys allows to specify what struct fields should be used as primary keys
-func WithPrimaryKeys(fields ...string) StructTransformerOption {
-	return func(t *structTransformer) {
-		t.pkFields = fields
-	}
-}
-
-// WithPrimaryKeyComponents allows to specify what struct fields should be used as primary key components
-func WithPrimaryKeyComponents(fields ...string) StructTransformerOption {
-	return func(t *structTransformer) {
-		t.pkComponentFields = fields
-	}
-}
-
-func TransformWithStruct(st any, opts ...StructTransformerOption) schema.Transform {
-	t := &structTransformer{
-		nameTransformer:          DefaultNameTransformer,
-		typeTransformer:          DefaultTypeTransformer,
-		resolverTransformer:      DefaultResolverTransformer,
-		ignoreInTestsTransformer: DefaultIgnoreInTestsTransformer,
-	}
-	for _, opt := range opts {
-		opt(t)
-	}
-
-	return func(table *schema.Table) error {
-		t.table = table
-		e := reflect.ValueOf(st)
-		if e.Kind() == reflect.Pointer {
-			e = e.Elem()
-		}
-		if e.Kind() == reflect.Slice {
-			e = reflect.MakeSlice(e.Type(), 1, 1).Index(0)
-		}
-		if e.Kind() != reflect.Struct {
-			return fmt.Errorf("expected struct, got %s", e.Kind())
-		}
-		eType := e.Type()
-		for i := 0; i < e.NumField(); i++ {
-			field := eType.Field(i)
-
-			switch {
-			case t.shouldUnwrapField(field):
-				if err := t.unwrapField(field); err != nil {
-					return err
-				}
-			default:
-				if err := t.addColumnFromField(field, nil); err != nil {
-					return fmt.Errorf("failed to add column for field %s: %w", field.Name, err)
-				}
-			}
-		}
-		// Validate that all expected PK fields were found
-		if diff := funk.SubtractString(t.pkFields, t.pkFieldsFound); len(diff) > 0 {
-			return fmt.Errorf("failed to create all of the desired primary keys: %v", diff)
-		}
-
-		if diff := funk.SubtractString(t.pkComponentFields, t.pkComponentFieldsFound); len(diff) > 0 {
-			return fmt.Errorf("failed to find all of the desired primary key components: %v", diff)
-		}
-		return nil
+func isTypeIgnored(t reflect.Type) bool {
+	switch t.Kind() {
+	case reflect.Func,
+		reflect.Chan,
+		reflect.UnsafePointer:
+		return true
+	default:
+		return false
 	}
 }
 
@@ -314,85 +184,52 @@ func (t *structTransformer) addColumnFromField(field reflect.StructField, parent
 	return nil
 }
 
-func isTypeIgnored(t reflect.Type) bool {
-	switch t.Kind() {
-	case reflect.Func,
-		reflect.Chan,
-		reflect.UnsafePointer:
-		return true
-	default:
-		return false
+func TransformWithStruct(st any, opts ...StructTransformerOption) schema.Transform {
+	t := &structTransformer{
+		nameTransformer:          DefaultNameTransformer,
+		typeTransformer:          DefaultTypeTransformer,
+		resolverTransformer:      DefaultResolverTransformer,
+		ignoreInTestsTransformer: DefaultIgnoreInTestsTransformer,
 	}
-}
-
-func DefaultTypeTransformer(v reflect.StructField) (arrow.DataType, error) {
-	return defaultGoTypeToSchemaType(v.Type)
-}
-
-func defaultGoTypeToSchemaType(v reflect.Type) (arrow.DataType, error) {
-	// Non primitive types
-	if v == reflect.TypeOf(net.IP{}) {
-		return types.ExtensionTypes.Inet, nil
+	for _, opt := range opts {
+		opt(t)
 	}
 
-	k := v.Kind()
-	switch k {
-	case reflect.Pointer:
-		return defaultGoTypeToSchemaType(v.Elem())
-	case reflect.String:
-		return arrow.BinaryTypes.String, nil
-	case reflect.Bool:
-		return arrow.FixedWidthTypes.Boolean, nil
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return arrow.PrimitiveTypes.Int64, nil
-	case reflect.Float32, reflect.Float64:
-		return arrow.PrimitiveTypes.Float64, nil
-	case reflect.Map:
-		return types.ExtensionTypes.JSON, nil
-	case reflect.Struct:
-		if v == reflect.TypeOf(time.Time{}) {
-			return arrow.FixedWidthTypes.Timestamp_us, nil
+	return func(table *schema.Table) error {
+		t.table = table
+		e := reflect.ValueOf(st)
+		if e.Kind() == reflect.Pointer {
+			e = e.Elem()
 		}
-		return types.ExtensionTypes.JSON, nil
-	case reflect.Slice:
-		switch v.Elem().Kind() {
-		case reflect.Uint8:
-			return arrow.BinaryTypes.Binary, nil
-		case reflect.Interface:
-			return types.ExtensionTypes.JSON, nil
+		if e.Kind() == reflect.Slice {
+			e = reflect.MakeSlice(e.Type(), 1, 1).Index(0)
+		}
+		if e.Kind() != reflect.Struct {
+			return fmt.Errorf("expected struct, got %s", e.Kind())
+		}
+		eType := e.Type()
+		for i := 0; i < e.NumField(); i++ {
+			field := eType.Field(i)
+
+			switch {
+			case t.shouldUnwrapField(field):
+				if err := t.unwrapField(field); err != nil {
+					return err
+				}
+			default:
+				if err := t.addColumnFromField(field, nil); err != nil {
+					return fmt.Errorf("failed to add column for field %s: %w", field.Name, err)
+				}
+			}
+		}
+		// Validate that all expected PK fields were found
+		if diff := funk.SubtractString(t.pkFields, t.pkFieldsFound); len(diff) > 0 {
+			return fmt.Errorf("failed to create all of the desired primary keys: %v", diff)
 		}
 
-		elemValueType, err := defaultGoTypeToSchemaType(v.Elem())
-		if err != nil {
-			return nil, err
+		if diff := funk.SubtractString(t.pkComponentFields, t.pkComponentFieldsFound); len(diff) > 0 {
+			return fmt.Errorf("failed to find all of the desired primary key components: %v", diff)
 		}
-		// if it's already JSON then we don't want to create list of JSON
-		if arrow.TypeEqual(elemValueType, types.ExtensionTypes.JSON) {
-			return elemValueType, nil
-		}
-		return arrow.ListOf(elemValueType), nil
-
-	case reflect.Interface:
-		return nil, nil // silently ignore
-
-	default:
-		return nil, fmt.Errorf("unsupported type: %s", k)
+		return nil
 	}
-}
-
-var defaultCaser = caser.New()
-
-func DefaultNameTransformer(field reflect.StructField) (string, error) {
-	name := field.Name
-	if jsonTag := strings.Split(field.Tag.Get("json"), ",")[0]; len(jsonTag) > 0 {
-		// return empty string if the field is not related api response
-		if jsonTag == "-" {
-			return "", nil
-		}
-		if nameFromJSONTag := defaultCaser.ToSnake(jsonTag); schema.ValidColumnName(nameFromJSONTag) {
-			return nameFromJSONTag, nil
-		}
-	}
-	return defaultCaser.ToSnake(name), nil
 }
