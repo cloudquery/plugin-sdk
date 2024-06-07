22b223e4a38ee0f02c126fea2c5b34cb579c7e79
candiduslynx <candiduslynx@gmail.com>
2024-06-07 09:54:39 +0300

    new scalar -> map

--- scalar/scalar.go
+++ scalar/scalar.go
@@ -133,6 +133,8 @@ func NewScalar(dt arrow.DataType) Scalar {
 
 	case arrow.STRUCT:
 		return &Struct{Type: dt.(*arrow.StructType)}
+	case arrow.MAP:
+		return &Map{Type: dt.(*arrow.MapType)}
 
 	case arrow.DECIMAL128:
 		return &Decimal128{Type: dt.(*arrow.Decimal128Type)}
