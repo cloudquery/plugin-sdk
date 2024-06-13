f19062f27a6608a98021c065664c0c6ddfbd7676
candiduslynx <candiduslynx@gmail.com>
2024-06-13 16:23:42 +0300

    Revert "Reapply "experiment with flush on demand""
    This reverts commit 8ab74d9391e784f6821f4d54efcb524e44f8d235.

--- scheduler/batch.go
+++ scheduler/batch.go
@@ -61,14 +61,6 @@ func (w *worker) work(done <-chan struct{}, timeout time.Duration) {
 				return
 			}
 
-			if r.TableDone() {
-				if w.curRows > 0 {
-					w.send()
-					ticker.Reset(timeout)
-				}
-				continue // this is just a flush after table resolver is done
-			}
-
 			// append to builder
 			scalar.AppendToRecordBuilder(w.builder, r.GetValues())
 			w.curRows++
