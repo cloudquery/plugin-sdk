381ef04e805e59fb38ab8e130e5472019faa6cf1
erezrokah <erezrokah@users.noreply.github.com>
2024-07-25 17:38:29 +0100

    style: Fix lint

--- helpers/retryablehttp/logger.go
+++ helpers/retryablehttp/logger.go
@@ -9,19 +9,19 @@ type leveledLogger struct {
 	zerolog.Logger
 }
 
-func (l *leveledLogger) Debug(msg string, keysAndValues ...interface{}) {
+func (l *leveledLogger) Debug(msg string, keysAndValues ...any) {
 	l.Logger.Debug().Fields(keysAndValues).Msg(msg)
 }
 
-func (l *leveledLogger) Error(msg string, keysAndValues ...interface{}) {
+func (l *leveledLogger) Error(msg string, keysAndValues ...any) {
 	l.Logger.Error().Fields(keysAndValues).Msg(msg)
 }
 
-func (l *leveledLogger) Info(msg string, keysAndValues ...interface{}) {
+func (l *leveledLogger) Info(msg string, keysAndValues ...any) {
 	l.Logger.Info().Fields(keysAndValues).Msg(msg)
 }
 
-func (l *leveledLogger) Warn(msg string, keysAndValues ...interface{}) {
+func (l *leveledLogger) Warn(msg string, keysAndValues ...any) {
 	l.Logger.Warn().Fields(keysAndValues).Msg(msg)
 }
 
