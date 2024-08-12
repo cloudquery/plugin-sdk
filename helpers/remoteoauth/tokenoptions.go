6613af3c564b6f1c38d90fed7a4b64af0bf7406d
Kemal Hadimli <disq@users.noreply.github.com>
2024-08-12 15:59:55 +0300

    Join all errors

--- helpers/remoteoauth/tokenoptions.go
+++ helpers/remoteoauth/tokenoptions.go
@@ -28,9 +28,11 @@ func (t *Token) initCloudOpts() error {
 		return nil
 	}
 
+	var allErr error
+
 	t.apiToken = os.Getenv("CLOUDQUERY_API_KEY")
 	if t.apiToken == "" {
-		return errors.New("CLOUDQUERY_API_KEY missing")
+		allErr = errors.Join(allErr, errors.New("CLOUDQUERY_API_KEY missing"))
 	}
 	t.apiURL = os.Getenv("CLOUDQUERY_API_URL")
 	if t.apiURL == "" {
@@ -39,47 +41,48 @@ func (t *Token) initCloudOpts() error {
 
 	t.teamName = os.Getenv("_CQ_TEAM_NAME")
 	if t.teamName == "" {
-		return errors.New("_CQ_TEAM_NAME missing")
+		allErr = errors.Join(allErr, errors.New("_CQ_TEAM_NAME missing"))
 	}
 	t.syncName = os.Getenv("_CQ_SYNC_NAME")
 	syncRunID := os.Getenv("_CQ_SYNC_RUN_ID")
 	testConnID := os.Getenv("_CQ_SYNC_TEST_CONNECTION_ID")
 	if testConnID == "" && syncRunID == "" {
-		return errors.New("_CQ_SYNC_TEST_CONNECTION_ID or _CQ_SYNC_RUN_ID missing")
+		allErr = errors.Join(allErr, errors.New("_CQ_SYNC_TEST_CONNECTION_ID or _CQ_SYNC_RUN_ID missing"))
 	} else if testConnID != "" && syncRunID != "" {
-		return errors.New("_CQ_SYNC_TEST_CONNECTION_ID and _CQ_SYNC_RUN_ID are mutually exclusive")
+		allErr = errors.Join(allErr, errors.New("_CQ_SYNC_TEST_CONNECTION_ID and _CQ_SYNC_RUN_ID are mutually exclusive"))
 	}
 
 	var err error
 	if syncRunID != "" {
 		if t.syncName == "" {
-			return errors.New("_CQ_SYNC_NAME missing")
+			allErr = errors.Join(allErr, errors.New("_CQ_SYNC_NAME missing"))
 		}
 
 		t.syncRunUUID, err = uuid.Parse(syncRunID)
 		if err != nil {
-			return fmt.Errorf("_CQ_SYNC_RUN_ID is not a valid UUID: %w", err)
+			allErr = errors.Join(allErr, fmt.Errorf("_CQ_SYNC_RUN_ID is not a valid UUID: %w", err))
 		}
 	}
 	if testConnID != "" {
 		if t.syncName != "" {
-			return errors.New("_CQ_SYNC_NAME should be empty")
+			allErr = errors.Join(allErr, errors.New("_CQ_SYNC_NAME should be empty"))
 		}
 
 		t.testConnUUID, err = uuid.Parse(testConnID)
 		if err != nil {
-			return fmt.Errorf("_CQ_SYNC_TEST_CONNECTION_ID is not a valid UUID: %w", err)
+			allErr = errors.Join(allErr, fmt.Errorf("_CQ_SYNC_TEST_CONNECTION_ID is not a valid UUID: %w", err))
 		}
 		t.isTestConnection = true
 	}
 
 	connectorID := os.Getenv("_CQ_CONNECTOR_ID")
 	if connectorID == "" {
-		return errors.New("_CQ_CONNECTOR_ID missing")
+		allErr = errors.Join(allErr, errors.New("_CQ_CONNECTOR_ID missing"))
+	} else {
+		t.connectorUUID, err = uuid.Parse(connectorID)
+		if err != nil {
+			allErr = errors.Join(allErr, fmt.Errorf("_CQ_CONNECTOR_ID is not a valid UUID: %w", err))
+		}
 	}
-	t.connectorUUID, err = uuid.Parse(connectorID)
-	if err != nil {
-		return fmt.Errorf("_CQ_CONNECTOR_ID is not a valid UUID: %w", err)
-	}
-	return nil
+	return allErr
 }
