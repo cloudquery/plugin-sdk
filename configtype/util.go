465e5d7a68e66cbcef68f6458717a41ee8181f8c
James Riley Wilburn <riley@cloudquery.io>
2024-09-26 10:30:15 -0400

    remove unused

--- configtype/util.go
+++ configtype/util.go
@@ -5,17 +5,3 @@ import "strings"
 func patternCases(cases ...string) string {
 	return "(" + strings.Join(cases, "|") + ")"
 }
-
-func abs(n int) int {
-	if n < 0 {
-		return -n
-	}
-	return n
-}
-
-func plural(n int) string {
-	if n != 1 {
-		return "s"
-	}
-	return ""
-}
